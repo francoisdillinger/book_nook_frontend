4a1a0137629b32fd633561e48e348f608f75ea79
francoisdillinger <elmojester@gmail.com>
2023-09-21 20:13:59 -0500

    Added screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
+  theme: {    
+    screens: {
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+  },
     extend: {},
   },
   plugins: [],
