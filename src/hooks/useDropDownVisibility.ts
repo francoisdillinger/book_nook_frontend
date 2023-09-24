e610e6f1a3387d9a7710148bc73ceb2af963fdad
francoisdillinger <elmojester@gmail.com>
2023-09-23 19:48:29 -0500

    Custom hook for profile dropdown

---
+++ src/hooks/useDropDownVisibility.ts
@@ -0,0 +1,62 @@
+import { useEffect, useReducer, useRef } from "react"
+
+interface State {
+    show: boolean;
+}
+
+interface Action {
+    type: 'dropDownOutOfFocus' | 'toggleDropDown' | 'navigating';
+}
+
+function reducer(state: State, action: Action){
+    const { type } = action;
+
+    switch (type) {
+        case 'dropDownOutOfFocus': {
+            return { show: false }
+        }
+        case 'toggleDropDown': {
+            return { show: !state.show }
+        }
+        case 'navigating': {
+            return { show: false }
+        }
+        default:
+            return state;
+    }
+}
+
+function useDropDownVisibility() {
+    const [state, dispatch] = useReducer(reducer, { show: true});
+    const profileRef = useRef<HTMLDivElement>(null);
+    const dropDownRef = useRef<HTMLDivElement>(null)
+    
+    useEffect(() => {
+        const dropDownFocusListener = (event: MouseEvent) => {
+            let profileRefClicked = profileRef.current && profileRef.current.contains(event.target as Node);
+            if(profileRefClicked){
+                dispatch({ type: 'toggleDropDown'});
+                return
+            }
+            
+            let dropDownInFocus = dropDownRef.current && dropDownRef.current.contains(event.target as Node)
+            if (!dropDownInFocus) dispatch({ type: 'dropDownOutOfFocus'});
+        }
+
+        document.addEventListener('mousedown', dropDownFocusListener);
+
+        return () => {
+            document.removeEventListener("mousedown", dropDownFocusListener);
+        }
+
+    }, []);
+
+    return {
+        state,
+        dispatch,
+        profileRef,
+        dropDownRef
+      };
+}
+
+export default useDropDownVisibility;
