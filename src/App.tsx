1cc2c5a2648b3d700587858d4a88a1b06f6631c4
francoisdillinger <elmojester@gmail.com>
2023-09-29 21:46:26 -0500

    Added RecomendedSlider to App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from "./components/Navbar";
 import HomeSection from './components/HomeSection';
 import HomeBadges from './components/HomeBadges';
+import RecomendedSlider from './components/RecomendedSlider';
+
 
 
 export default function App() {
@@ -9,6 +11,7 @@ export default function App() {
     <React.Fragment>
       <Navbar />
       <HomeSection />
+      <RecomendedSlider />
       <HomeBadges />
     </React.Fragment>
   )
