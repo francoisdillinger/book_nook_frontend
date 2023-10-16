cc1ec4ea49f9fab23ff61bed387bdfa441ee88df
francoisdillinger <elmojester@gmail.com>
2023-10-15 20:22:37 -0500

    Initial image rendering

--- src/components/HomeTestimonials.tsx
+++ src/components/HomeTestimonials.tsx
@@ -1,5 +1,10 @@
+import { books } from "../data/books"
+
+
 export default function HomeTestimonials(){
     return (
-        <div>Tesimonials</div>
+        <div>
+            <img className="w-20" src={books[0].image} alt="" />
+        </div>
     )
 }
