2854b23addd18a7863f9aa3654e3dfcc091b260e
francoisdillinger <elmojester@gmail.com>
2023-10-22 22:03:19 -0500

    Working on StarRating component

--- src/components/HomeTestimonials.tsx
+++ src/components/HomeTestimonials.tsx
@@ -1,10 +1,12 @@
 import { books } from "../data/books"
+import StarRating from "./StarRating"
 
 
 export default function HomeTestimonials(){
     return (
         <div>
-            <img className="w-20" src={books[0].image} alt="" />
+            {/* <img className="w-20" src={books[0].image} alt="" /> */}
+            < StarRating />
         </div>
     )
 }
