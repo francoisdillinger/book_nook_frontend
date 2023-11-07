e11a4d88506e2876ed8c05c5671342d9ffa1cfa5
francoisdillinger <elmojester@gmail.com>
2023-11-07 00:20:08 -0600

    Dummy data for recent testimonials

---
+++ src/data/recentTestimonials.ts
@@ -0,0 +1,16 @@
+import dracula from '../assets/dracula.jpg'
+import frankenstein from '../assets/frankenstein.jpg'
+import norseGods from '../assets/norse_mythology.jpg'
+import memers from '../assets/memers.jpg'
+import marms from '../assets/marms.jpg'
+import purpur from '../assets/purpur.jpg'
+
+
+export const testimonials = [
+    {bookImage: dracula, testimonial: "Dracula", rating: 3, user: 'Memow Jester', userImage: memers},
+    {bookImage: frankenstein, testimonial: "Frankenstein", rating: 4, user:'Marmalade Sipe', userImage: marms},
+    {bookImage: norseGods, testimonial: "Norse Gods", rating: 5, user:'Purpur Jester', userImage: purpur},
+    {bookImage: dracula, testimonial: "Dracula", rating: 3, user: 'Memow Jester', userImage: memers},
+    {bookImage: frankenstein, testimonial: "Frankenstein", rating: 4, user:'Marmalade Sipe', userImage: marms},
+    {bookImage: norseGods, testimonial: "Norse Gods", rating: 5, user:'Purpur Jester', userImage: purpur},
+]
