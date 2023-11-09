ede1044b59d989aa4ea8400d3a77df9deab427b5
francoisdillinger <elmojester@gmail.com>
2023-11-08 20:26:31 -0600

    recentArrivals dummy data

---
+++ src/data/recentArrivals.ts
@@ -0,0 +1,16 @@
+import dracula from '../assets/dracula.jpg'
+import frankenstein from '../assets/frankenstein.jpg'
+import norseGods from '../assets/norse_mythology.jpg'
+
+export const books = [
+    {image: dracula, title: "Dracula", price: "$5.99"},
+    {image: frankenstein, title: "Frankenstein", price: "$7.99"},
+    {image: norseGods, title: "Norse Gods", price: "$8.99"},
+    {image: dracula, title: "Dracula", price: "$5.99"},
+    {image: frankenstein, title: "Frankenstein", price: "$7.99"},
+    {image: norseGods, title: "Norse Gods", price: "$8.99"},
+    {image: dracula, title: "Dracula", price: "$5.99"},
+    {image: frankenstein, title: "Frankenstein", price: "$7.99"},
+    {image: norseGods, title: "Norse Gods", price: "$8.99"},
+    {image: dracula, title: "Dracula", price: "$5.99"},
+]
