854452983675b39efadfda971067e43f26a6af67
francoisdillinger <elmojester@gmail.com>
2023-11-12 19:51:06 -0600

    categories slider dummy data

---
+++ src/data/categories.ts
@@ -0,0 +1,14 @@
+export const categories = [
+    {"category": "Thriller", "numOfBooks": 38},
+    {"category": "Horror", "numOfBooks": 11},
+    {"category": "Non-Fiction", "numOfBooks": 6},
+    {"category": "Young Adult", "numOfBooks": 44},
+    {"category": "Biography", "numOfBooks": 31},
+    {"category": "Fantasy", "numOfBooks": 12},
+    {"category": "Historical", "numOfBooks": 48},
+    {"category": "Children", "numOfBooks": 43},
+    {"category": "Mystery", "numOfBooks": 27},
+    {"category": "Self-Help", "numOfBooks": 14},
+    {"category": "Science Fiction", "numOfBooks": 4},
+    {"category": "Romance", "numOfBooks": 27}
+]
