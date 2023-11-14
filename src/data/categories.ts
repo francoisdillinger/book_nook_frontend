224c853f7d10425e4d3eb88802d5b76b949533cd
francoisdillinger <elmojester@gmail.com>
2023-11-13 21:01:30 -0600

    categories dummy data

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,14 +1,21 @@
+import dracula from '../assets/dracula.jpg'
+import frankenstein from '../assets/frankenstein.jpg'
+import norseGods from '../assets/norse_mythology.jpg'
+import alchemist from '../assets/alchemist.jpg'
+import sleepy from '../assets/sleepy.jpg'
+import snow from '../assets/snow.jpg'
+
 export const categories = [
-    {"category": "Thriller", "numOfBooks": 38},
-    {"category": "Horror", "numOfBooks": 11},
-    {"category": "Non-Fiction", "numOfBooks": 6},
-    {"category": "Young Adult", "numOfBooks": 44},
-    {"category": "Biography", "numOfBooks": 31},
-    {"category": "Fantasy", "numOfBooks": 12},
-    {"category": "Historical", "numOfBooks": 48},
-    {"category": "Children", "numOfBooks": 43},
-    {"category": "Mystery", "numOfBooks": 27},
-    {"category": "Self-Help", "numOfBooks": 14},
-    {"category": "Science Fiction", "numOfBooks": 4},
-    {"category": "Romance", "numOfBooks": 27}
+    {"category": "Thriller", "numOfBooks": 38, image: snow},
+    {"category": "Horror", "numOfBooks": 11, image: sleepy},
+    {"category": "Non-Fiction", "numOfBooks": 6, image: alchemist},
+    {"category": "Young Adult", "numOfBooks": 44, image: norseGods},
+    {"category": "Biography", "numOfBooks": 31, image: frankenstein},
+    {"category": "Fantasy", "numOfBooks": 12, image: dracula},
+    {"category": "Historical", "numOfBooks": 48, image: snow},
+    {"category": "Children", "numOfBooks": 43, image: sleepy},
+    {"category": "Mystery", "numOfBooks": 27, image: alchemist},
+    {"category": "Self-Help", "numOfBooks": 14, image: norseGods},
+    {"category": "Science Fiction", "numOfBooks": 4, image: frankenstein},
+    {"category": "Romance", "numOfBooks": 27, image: dracula}
 ]
