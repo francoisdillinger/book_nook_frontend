9054fd110883c447b244aa26c135ddc9ba267547
francoisdillinger <elmojester@gmail.com>
2023-11-29 10:39:35 -0600

    Prettier Formatting

--- src/components/HomePageContent.tsx
+++ src/components/HomePageContent.tsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import BlogSection from './BlogSection'
-import CategoriesSlider from './CategoriesSlider'
-import HomeBadges from './HomeBadges'
-import HomeBookTestimonials from './HomeBookTestimonials'
-import HomeSection from './HomeSection'
-import HomeStatsBadges from './HomeStatsBadges'
-import NewArrivals from './NewArrivals'
-import NewsLetter from './NewsLetter'
-import RecommendedSlider from './RecommendedSlider'
+import React from "react";
+import BlogSection from "./BlogSection";
+import CategoriesSlider from "./CategoriesSlider";
+import HomeBadges from "./HomeBadges";
+import HomeBookTestimonials from "./HomeBookTestimonials";
+import HomeSection from "./HomeSection";
+import HomeStatsBadges from "./HomeStatsBadges";
+import NewArrivals from "./NewArrivals";
+import NewsLetter from "./NewsLetter";
+import RecommendedSlider from "./RecommendedSlider";
 
-export default function HomePageContent(){
-    return(
-        <React.Fragment>
-            <HomeSection />
-            <NewArrivals />
-            <HomeBadges />
-            <CategoriesSlider />
-            <RecommendedSlider />
-            <HomeBookTestimonials />
-            <HomeStatsBadges />
-            <BlogSection />
-            <NewsLetter />
-        </React.Fragment>
-    )
+export default function HomePageContent() {
+	return (
+		<React.Fragment>
+			<HomeSection />
+			<NewArrivals />
+			<HomeBadges />
+			<CategoriesSlider />
+			<RecommendedSlider />
+			<HomeBookTestimonials />
+			<HomeStatsBadges />
+			<BlogSection />
+			<NewsLetter />
+		</React.Fragment>
+	);
 }
