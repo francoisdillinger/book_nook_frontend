230ee08c6b365c423c7b3a443492a3ee0ab57a49
francoisdillinger <elmojester@gmail.com>
2023-11-28 23:14:06 -0600

    Prettier Formatting

--- src/data/recentArrivals.ts
+++ src/data/recentArrivals.ts
@@ -1,21 +1,21 @@
-import dracula from '../assets/dracula.jpg'
-import frankenstein from '../assets/frankenstein.jpg'
-import norseGods from '../assets/norse_mythology.jpg'
-import alchemist from '../assets/alchemist.jpg'
-import sleepy from '../assets/sleepy.jpg'
-import snow from '../assets/snow.jpg'
+import dracula from "../assets/dracula.jpg";
+import frankenstein from "../assets/frankenstein.jpg";
+import norseGods from "../assets/norse_mythology.jpg";
+import alchemist from "../assets/alchemist.jpg";
+import sleepy from "../assets/sleepy.jpg";
+import snow from "../assets/snow.jpg";
 
 export const recentArrivals = [
-    {image: dracula, title: "Dracula", price: "$5.99"},
-    {image: alchemist, title: "The Alchemist", price: "$18.99"},
-    {image: frankenstein, title: "Frankenstein", price: "$7.99"},
-    {image: sleepy, title: "Sleepy Hollow", price: "$6.99"},
-    {image: norseGods, title: "Norse Gods", price: "$8.99"},
-    {image: snow, title: "Snow Crash", price: "$12.99"},
-    {image: dracula, title: "Dracula", price: "$5.99"},
-    {image: alchemist, title: "The Alchemist", price: "$18.99"},
-    {image: frankenstein, title: "Frankenstein", price: "$7.99"},
-    {image: sleepy, title: "Sleepy Hollow", price: "$6.99"},
-    {image: norseGods, title: "Norse Gods", price: "$8.99"},
-    {image: snow, title: "Snow Crash", price: "$12.99"},
-]
+	{ image: dracula, title: "Dracula", price: "$5.99" },
+	{ image: alchemist, title: "The Alchemist", price: "$18.99" },
+	{ image: frankenstein, title: "Frankenstein", price: "$7.99" },
+	{ image: sleepy, title: "Sleepy Hollow", price: "$6.99" },
+	{ image: norseGods, title: "Norse Gods", price: "$8.99" },
+	{ image: snow, title: "Snow Crash", price: "$12.99" },
+	{ image: dracula, title: "Dracula", price: "$5.99" },
+	{ image: alchemist, title: "The Alchemist", price: "$18.99" },
+	{ image: frankenstein, title: "Frankenstein", price: "$7.99" },
+	{ image: sleepy, title: "Sleepy Hollow", price: "$6.99" },
+	{ image: norseGods, title: "Norse Gods", price: "$8.99" },
+	{ image: snow, title: "Snow Crash", price: "$12.99" },
+];
