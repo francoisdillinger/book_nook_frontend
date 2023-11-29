ab812cd019371a36d592a3b32237c7a7711353cf
francoisdillinger <elmojester@gmail.com>
2023-11-28 23:12:40 -0600

    Formatting with prettier

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,21 +1,21 @@
-import dracula from '../assets/dracula.jpg'
-import frankenstein from '../assets/frankenstein.jpg'
-import norseGods from '../assets/norse_mythology.jpg'
-import alchemist from '../assets/alchemist.jpg'
-import sleepy from '../assets/sleepy.jpg'
-import snow from '../assets/snow.jpg'
+import dracula from "../assets/dracula.jpg";
+import frankenstein from "../assets/frankenstein.jpg";
+import norseGods from "../assets/norse_mythology.jpg";
+import alchemist from "../assets/alchemist.jpg";
+import sleepy from "../assets/sleepy.jpg";
+import snow from "../assets/snow.jpg";
 
 export const categories = [
-    {"category": "Thriller", "numOfBooks": 38, image: snow},
-    {"category": "Horror", "numOfBooks": 11, image: sleepy},
-    {"category": "Non-Fiction", "numOfBooks": 6, image: alchemist},
-    {"category": "Young Adult", "numOfBooks": 44, image: norseGods},
-    {"category": "Biography", "numOfBooks": 31, image: frankenstein},
-    {"category": "Fantasy", "numOfBooks": 12, image: dracula},
-    {"category": "Historical", "numOfBooks": 48, image: snow},
-    {"category": "Children", "numOfBooks": 43, image: sleepy},
-    {"category": "Mystery", "numOfBooks": 27, image: alchemist},
-    {"category": "Self-Help", "numOfBooks": 14, image: norseGods},
-    {"category": "Science Fiction", "numOfBooks": 4, image: frankenstein},
-    {"category": "Romance", "numOfBooks": 27, image: dracula}
-]
+	{ category: "Thriller", numOfBooks: 38, image: snow },
+	{ category: "Horror", numOfBooks: 11, image: sleepy },
+	{ category: "Non-Fiction", numOfBooks: 6, image: alchemist },
+	{ category: "Young Adult", numOfBooks: 44, image: norseGods },
+	{ category: "Biography", numOfBooks: 31, image: frankenstein },
+	{ category: "Fantasy", numOfBooks: 12, image: dracula },
+	{ category: "Historical", numOfBooks: 48, image: snow },
+	{ category: "Children", numOfBooks: 43, image: sleepy },
+	{ category: "Mystery", numOfBooks: 27, image: alchemist },
+	{ category: "Self-Help", numOfBooks: 14, image: norseGods },
+	{ category: "Science Fiction", numOfBooks: 4, image: frankenstein },
+	{ category: "Romance", numOfBooks: 27, image: dracula },
+];
