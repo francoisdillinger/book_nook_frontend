d403a7ea92598754b9a1d9757661e6afa098a991
francoisdillinger <elmojester@gmail.com>
2023-12-18 00:21:24 -0600

    created booksales dummy data

---
+++ src/data/bookSales.ts
@@ -0,0 +1,14 @@
+export const bookSales = [
+	{ title: "Frankenstein", month: 1, sold: 8 },
+	{ title: "Frankenstein", month: 2, sold: 30 },
+	{ title: "Frankenstein", month: 3, sold: 29 },
+	{ title: "Frankenstein", month: 4, sold: 26 },
+	{ title: "Frankenstein", month: 5, sold: 7 },
+	{ title: "Frankenstein", month: 6, sold: 25 },
+	{ title: "Frankenstein", month: 7, sold: 7 },
+	{ title: "Frankenstein", month: 8, sold: 1 },
+	{ title: "Frankenstein", month: 9, sold: 50 },
+	{ title: "Frankenstein", month: 10, sold: 20 },
+	{ title: "Frankenstein", month: 11, sold: 5 },
+	{ title: "Frankenstein", month: 12, sold: 34 },
+];
