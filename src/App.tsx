b7bf5a27e3e04a58eaff849e83ffe5b8a8c1cec1
francoisdillinger <elmojester@gmail.com>
2023-12-30 19:08:29 -0600

    Refactor component imports and remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
+import React from "react";
 import Navbar from "./components/Navbar";
-import HomeSection from './components/HomeSection';
-import HomeBadges from './components/HomeBadges';
-import RecommendedSlider from './components/RecommendedSlider';
-import HomeBookTestimonials from './components/HomeBookTestimonials';
-import NewArrivals from './components/NewArrivals';
-import Footer from './components/Footer';
-import CategoriesSlider from './components/CategoriesSlider';
-import HomeStatsBadges from './components/HomeStatsBadges';
-
-
+import HomeSection from "./components/HomeSection";
+import HomeBadges from "./components/HomeBadges";
+import RecommendedSlider from "./components/RecommendedSlider";
+import HomeBookTestimonials from "./components/HomeBookTestimonials";
+import NewArrivals from "./components/NewArrivals";
+import Footer from "./components/Footer";
+import CategoriesSlider from "./components/CategoriesSlider";
+import HomeStatsBadges from "./components/HomeStatsBadges";
+import NewsLetter from "./components/NewsLetter";
+import BlogSection from "./components/BlogSection";
+import HomePageContent from "./components/HomePageContent";
+import SideBarAdminMenu from "./components/SideBarAdminMenu";
+import AdminHome from "./components/AdminHome";
+import Scratch from "./components/Scratch";
 
 export default function App() {
-  return (
-    <React.Fragment>
-      {/* <Navbar />
-      <HomeSection />
-      <NewArrivals />
-      <HomeBadges />
-      <CategoriesSlider />
-      <RecommendedSlider />
-      <HomeBookTestimonials /> */}
-      <HomeStatsBadges />
-      {/* <Footer /> */}
-    </React.Fragment>
-  )
+	return (
+		<React.Fragment>
+			{/* <Navbar /> */}
+			{/* <HomePageContent /> */}
+			{/* <SideBarAdminMenu /> */}
+			<AdminHome />
+			{/* <Scratch /> */}
+			{/* <Footer /> */}
+		</React.Fragment>
+	);
 }
