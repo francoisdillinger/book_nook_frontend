ec209c7a5cc60c7b79e8587edff95fedce5ce070
francoisdillinger <elmojester@gmail.com>
2024-01-02 21:05:40 -0600

    Add bar chart to AdminHome component

--- src/components/AdminHome.tsx
+++ src/components/AdminHome.tsx
@@ -7,16 +7,54 @@ export default function AdminHome() {
 
 	useEffect(() => {
 		if (svgRef.current) {
+			const margin = { top: 20, right: 20, bottom: 100, left: 100 };
+			const graphWidth = 800 - margin.left - margin.right;
+			const graphHeight = 600 - margin.top - margin.bottom;
+
 			const svg = d3.select(svgRef.current);
-			// console.log("hello");
-			const rects = svg.selectAll("rect").data(bookSales);
+			const graph = svg
+				.append("g")
+				.attr("width", graphWidth)
+				.attr("height", graphHeight)
+				.attr("transform", `translate(${margin.left},${margin.top})`);
+
+			const xAxisGroup = graph
+				.append("g")
+				.attr("transform", `translate(0,${graphHeight})`);
+			const yAxisGroup = graph.append("g");
+
+			const max = d3.max(bookSales, (d) => d.sold);
+			const y = d3
+				.scaleLinear()
+				.domain([0, max as number])
+				.range([graphHeight, 0]);
+
+			const x = d3
+				.scaleBand()
+				.domain(bookSales.map((book) => book.month.toString()))
+				.range([0, graphWidth])
+				.paddingInner(0.2)
+				.paddingOuter(0.2);
+
+			const rects = graph.selectAll("rect").data(bookSales);
 
 			rects
 				.enter()
 				.append("rect")
-				.attr("width", 20)
-				.attr("height", (d) => d.sold)
-				.attr("fill", "blue");
+				.attr("width", x.bandwidth)
+				.attr("height", (d) => graphHeight - y(d.sold))
+				.attr("fill", "rgb(8 145 178)")
+				.attr("x", (d) => x(d.month.toString()) ?? 0)
+				.attr("y", (d) => y(d.sold));
+
+			const xAxis = d3.axisBottom(x);
+			const yAxis = d3
+				.axisLeft(y)
+				.ticks(5)
+				.tickFormat((d) => d + " orders");
+
+			xAxisGroup.call(xAxis);
+			yAxisGroup.call(yAxis);
 		}
 	}, []);
 
@@ -25,8 +63,8 @@ export default function AdminHome() {
 			<div>
 				<svg
 					ref={svgRef}
-					width={1200}
-					height={1000}
+					width={800}
+					height={600}
 				>
 					{/* {bookSales.map((book, i) => (
 						<rect
