454ffd40409f3a8b97e206078d3eec3b54bcb9f4
francoisdillinger <elmojester@gmail.com>
2024-01-04 22:07:02 -0600

    Add book sales data with dates

---
+++ src/data/bookSalesWithDate.ts
@@ -0,0 +1,14 @@
+export const books = [
+	{ title: "Frankenstein", date: "2023-01-23", sold: 28 },
+	{ title: "Frankenstein", date: "2023-02-14", sold: 28 },
+	{ title: "Frankenstein", date: "2023-03-11", sold: 34 },
+	{ title: "Frankenstein", date: "2023-04-19", sold: 19 },
+	{ title: "Frankenstein", date: "2023-05-21", sold: 40 },
+	{ title: "Frankenstein", date: "2023-06-26", sold: 25 },
+	{ title: "Frankenstein", date: "2023-07-03", sold: 5 },
+	{ title: "Frankenstein", date: "2023-08-04", sold: 5 },
+	{ title: "Frankenstein", date: "2023-09-01", sold: 32 },
+	{ title: "Frankenstein", date: "2023-10-17", sold: 12 },
+	{ title: "Frankenstein", date: "2023-11-24", sold: 25 },
+	{ title: "Frankenstein", date: "2023-12-28", sold: 2 },
+];
