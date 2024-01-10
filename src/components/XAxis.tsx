8f8c4c8bae03315ba898964c7c2492707aeff1eb
francoisdillinger <elmojester@gmail.com>
2024-01-09 23:09:27 -0600

    Add XAxis component

---
+++ src/components/XAxis.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef } from "react";
+import { axisBottom } from "d3-axis";
+import { select } from "d3-selection";
+import * as d3 from "d3";
+import { ScaleLinear, ScaleBand } from "d3-scale";
+import { AxisScale } from "d3-axis";
+import { ScaleTime } from "d3";
+
+type XAxisTypes = {
+	xScale: ScaleTime<number, number>;
+	height: number;
+};
+
+const XAxis: React.FC<XAxisTypes> = ({ xScale, height }) => {
+	const ref = useRef<SVGGElement>(null);
+
+	useEffect(() => {
+		const xScaleNumeric = xScale as unknown as AxisScale<number>;
+
+		const xAxis = axisBottom(xScaleNumeric).ticks(12);
+		// Additional customizations if needed, e.g., .ticks(5)
+
+		const transition = select(ref.current)
+			.transition()
+			.duration(500) // Duration of the transition in milliseconds
+			.ease(d3.easeCubicInOut); // Easing function
+
+		transition.call(xAxis as any);
+	}, [xScale]);
+
+	return (
+		<g
+			ref={ref}
+			className="text-neutral-600"
+			transform={`translate(0,${height})`}
+		/>
+	);
+};
+
+export default XAxis;
