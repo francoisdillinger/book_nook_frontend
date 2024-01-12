3df76b7471821082508c4f74077e73a58ebc44c5
francoisdillinger <elmojester@gmail.com>
2024-01-11 23:29:37 -0600

    Added chart legend

--- src/components/UsersAdminChart.tsx
+++ src/components/UsersAdminChart.tsx
@@ -120,6 +120,8 @@ const getFilteredData = (filter) => {
 export default function UsersAdminChart() {
 	// const windowSize = useGetWindowSize();
 	// console.log(windowSize);
+	const [focusedUser, setFocusedUser] = useState("");
+	console.log(focusedUser);
 	const [windowSizeInPixels, setWindowSizeInPixels] = useState(
 		window.innerWidth
 	);
@@ -172,8 +174,11 @@ export default function UsersAdminChart() {
 
 	const svgRef = useRef<SVGSVGElement>(null);
 	const graphRef = useRef<SVGSVGElement>(null);
-	const margin = { top: 20, right: 20, bottom: 100, left: 80 };
-	const graphWidth = windowSizeInPixels * 0.75 - margin.left - margin.right;
+	const margin = { top: 20, right: 20, bottom: 100, left: 40 };
+	const graphWidth =
+		windowSizeInPixels <= 800
+			? windowSizeInPixels - margin.left - margin.right
+			: windowSizeInPixels * 0.85 - margin.left - margin.right;
 	const graphHeight = 600 - margin.top - margin.bottom;
 	const max = d3.max(allQuantities);
 	const colorScale = d3.scaleOrdinal(d3.schemeTableau10);
@@ -195,205 +200,273 @@ export default function UsersAdminChart() {
 	// if (filteredUserchart.length === 0) {
 	// 	console.log("Emtpy!");
 	// }
+	useEffect(() => {
+		const onGlobalClick = () => {
+			setFocusedUser("");
+		};
+
+		window.addEventListener("click", onGlobalClick);
+
+		return () => {
+			window.removeEventListener("click", onGlobalClick);
+		};
+	}, []);
+
+	const onUsernameClick = (username, event) => {
+		event.stopPropagation(); // Prevent the click from reaching the global listener
+		setFocusedUser(username);
+	};
 	return (
 		<React.Fragment>
-			<div className="">
-				<div className="ml-12">
-					<button
-						className={`p-2 w-16 ${
-							timeFilter === "day"
-								? "bg-slate-100 rounded-full text-logo shadow-md "
-								: "text-neutral-600"
-						}`}
-						onClick={() => setTimeFilter("day")}
-					>
-						1D
-					</button>
-					<button
-						className={`p-2 w-16 ${
-							timeFilter === "week"
-								? "bg-slate-100 rounded-full text-logo shadow-md"
-								: "text-neutral-600"
-						}`}
-						onClick={() => setTimeFilter("week")}
-					>
-						1W
-					</button>
-					<button
-						className={`p-2 w-16 ${
-							timeFilter === "month"
-								? "bg-slate-100 rounded-full text-logo shadow-md"
-								: "text-neutral-600"
-						}`}
-						onClick={() => setTimeFilter("month")}
-					>
-						1M
-					</button>
-					<button
-						className={`p-2 w-16 ${
-							timeFilter === "half-year"
-								? "bg-slate-100 rounded-full text-logo shadow-md"
-								: "text-neutral-600"
-						}`}
-						onClick={() => setTimeFilter("half-year")}
-					>
-						6M
-					</button>
-					<button
-						className={`p-2 w-16 ${
-							timeFilter === "year"
-								? "bg-slate-100 rounded-full text-logo shadow-md"
-								: "text-neutral-600"
-						}`}
-						onClick={() => setTimeFilter("year")}
-					>
-						YTD
-					</button>
-					<button
-						className={`p-2 w-16 ${
-							timeFilter === "max"
-								? "bg-slate-100 rounded-full text-logo shadow-md"
-								: "text-neutral-600"
-						}`}
-						onClick={() => setTimeFilter("max")}
-					>
-						Max
-					</button>
-				</div>
-
+			<div className="flex">
 				<div
-					className="absolute bg-white rounded-md p-2 text-slate-500 font-light text-xs shadow-2xl"
-					style={{
-						display: tooltip.visible ? "block" : "none",
-						left: `${tooltip.x}px`,
-						top: `${tooltip.y}px`,
-					}}
+					className=""
+					style={{ width: windowSizeInPixels * 0.85 }}
 				>
-					{tooltip.content}
-				</div>
-				<svg
-					ref={svgRef}
-					width={windowSizeInPixels}
-					height={600}
-				>
-					<g
-						ref={graphRef}
-						width={graphWidth}
-						height={graphHeight}
-						transform={`translate(${margin.left},${margin.top})`}
-					>
-						<XAxis
-							xScale={x}
-							height={graphHeight}
-						/>
-						<YAxis
-							yScale={y}
-							graphWidth={graphWidth}
-							hasData={hasData}
-						/>
-						{hasData ? (
-							filteredUserchart.map(
-								(
-									user: {
-										orders: any[] | Iterable<[number, number]>;
-										userName: React.Key | null | undefined;
-									},
-									index: { toString: () => string }
-								) => {
-									const color = colorScale(index.toString());
-									const linePath = theLine(
-										user.orders.map((order: { date: any }) => ({
-											...order,
-											date: order.date,
-										}))
-									);
+					<div className="ml-4 h-8">
+						<button
+							className={`p-2 w-16 ${
+								timeFilter === "day"
+									? "bg-slate-100 rounded-full text-logo shadow-md "
+									: "text-neutral-600"
+							}`}
+							onClick={() => setTimeFilter("day")}
+						>
+							1D
+						</button>
+						<button
+							className={`p-2 w-16 ${
+								timeFilter === "week"
+									? "bg-slate-100 rounded-full text-logo shadow-md"
+									: "text-neutral-600"
+							}`}
+							onClick={() => setTimeFilter("week")}
+						>
+							1W
+						</button>
+						<button
+							className={`p-2 w-16 ${
+								timeFilter === "month"
+									? "bg-slate-100 rounded-full text-logo shadow-md"
+									: "text-neutral-600"
+							}`}
+							onClick={() => setTimeFilter("month")}
+						>
+							1M
+						</button>
+						<button
+							className={`p-2 w-16 ${
+								timeFilter === "half-year"
+									? "bg-slate-100 rounded-full text-logo shadow-md"
+									: "text-neutral-600"
+							}`}
+							onClick={() => setTimeFilter("half-year")}
+						>
+							6M
+						</button>
+						<button
+							className={`p-2 w-16 ${
+								timeFilter === "year"
+									? "bg-slate-100 rounded-full text-logo shadow-md"
+									: "text-neutral-600"
+							}`}
+							onClick={() => setTimeFilter("year")}
+						>
+							YTD
+						</button>
+						<button
+							className={`p-2 w-16 ${
+								timeFilter === "max"
+									? "bg-slate-100 rounded-full text-logo shadow-md"
+									: "text-neutral-600"
+							}`}
+							onClick={() => setTimeFilter("max")}
+						>
+							Max
+						</button>
+					</div>
 
-									return (
-										<React.Fragment key={user.userName}>
-											{/* Unique key for each fragment */}
-											<motion.path
-												key={user.orders}
-												initial={{ d: bottomLineGenerator(user.orders) }}
-												animate={{ d: linePath }}
-												transition={{ ease: "easeInOut", duration: 0.5 }}
-												fill="none"
-												strokeWidth={2}
-												stroke={color}
-											/>
-											{user.orders.map((order: { orderId; quantity; date }) => (
-												<motion.circle
-													key={order.orderId}
-													className="cursor-pointer"
-													initial={{ cy: graphHeight }}
-													animate={{
-														cy: y(order.quantity),
-														cx: x(new Date(order.date)),
-													}}
+					<div
+						className="absolute bg-white rounded-md p-2 text-slate-500 font-light text-xs shadow-2xl"
+						style={{
+							display: tooltip.visible ? "block" : "none",
+							left: `${tooltip.x}px`,
+							top: `${tooltip.y}px`,
+						}}
+					>
+						{tooltip.content}
+					</div>
+					<svg
+						ref={svgRef}
+						width={windowSizeInPixels}
+						height={600}
+					>
+						<g
+							ref={graphRef}
+							width={graphWidth}
+							height={graphHeight}
+							transform={`translate(${margin.left},${margin.top})`}
+						>
+							<XAxis
+								xScale={x}
+								height={graphHeight}
+							/>
+							<YAxis
+								yScale={y}
+								graphWidth={graphWidth}
+								hasData={hasData}
+							/>
+							{hasData ? (
+								filteredUserchart.map(
+									(
+										user: {
+											orders: any[] | Iterable<[number, number]>;
+											userName: React.Key | null | undefined;
+										},
+										index: { toString: () => string }
+									) => {
+										const color = colorScale(index.toString());
+										const linePath = theLine(
+											user.orders.map((order: { date: any }) => ({
+												...order,
+												date: order.date,
+											}))
+										);
+
+										return (
+											<React.Fragment key={user.userName}>
+												{/* Unique key for each fragment */}
+												<motion.path
+													key={user.orders}
+													initial={{ d: bottomLineGenerator(user.orders) }}
+													animate={{ d: linePath }}
 													transition={{ ease: "easeInOut", duration: 0.5 }}
-													r={6}
-													fill={color}
-													onMouseEnter={(e) => {
-														console.log("We in here");
-														const content = (
-															<div>
-																<div>
-																	<span className="text-slate-600 font-bold">
-																		Order ID:
-																	</span>{" "}
-																	{order.orderId}
-																</div>
-																<div>
-																	<span className="text-slate-600 font-bold">
-																		Date:
-																	</span>{" "}
-																	{order.date.toDateString()}
-																</div>
-																<div>
-																	<span className="text-slate-600 font-bold">
-																		Quantity:
-																	</span>{" "}
-																	{order.quantity}
-																</div>
-															</div>
-														);
-														setTooltip({
-															visible: true,
-															content: content,
-															x: e.clientX,
-															y: e.clientY,
-														});
-													}}
-													onMouseLeave={() => {
-														setTooltip({ ...tooltip, visible: false });
-													}}
+													fill="none"
+													strokeWidth={2}
+													stroke={
+														focusedUser === user.userName || focusedUser === ""
+															? color
+															: "gray"
+													}
+													opacity={
+														focusedUser === user.userName || focusedUser === ""
+															? 1
+															: 0.2
+													}
 												/>
-											))}
-										</React.Fragment>
-									);
-								}
-							)
+												{user.orders.map(
+													(order: { orderId; quantity; date }) => (
+														<motion.circle
+															key={order.orderId}
+															className="cursor-pointer"
+															initial={{ cy: graphHeight }}
+															animate={{
+																cy: y(order.quantity),
+																cx: x(new Date(order.date)),
+															}}
+															transition={{ ease: "easeInOut", duration: 0.5 }}
+															r={6}
+															fill={
+																focusedUser === user.userName ||
+																focusedUser === ""
+																	? color
+																	: "gray"
+															}
+															opacity={
+																focusedUser === user.userName ||
+																focusedUser === ""
+																	? 1
+																	: 0.2
+															}
+															onMouseEnter={(e) => {
+																console.log("We in here");
+																const content = (
+																	<div>
+																		<div>
+																			<span className="text-slate-600 font-bold">
+																				Order ID:
+																			</span>{" "}
+																			{order.orderId}
+																		</div>
+																		<div>
+																			<span className="text-slate-600 font-bold">
+																				Date:
+																			</span>{" "}
+																			{order.date.toDateString()}
+																		</div>
+																		<div>
+																			<span className="text-slate-600 font-bold">
+																				Quantity:
+																			</span>{" "}
+																			{order.quantity}
+																		</div>
+																	</div>
+																);
+																setTooltip({
+																	visible: true,
+																	content: content,
+																	x: e.clientX,
+																	y: e.clientY,
+																});
+															}}
+															onMouseLeave={() => {
+																setTooltip({ ...tooltip, visible: false });
+															}}
+														/>
+													)
+												)}
+											</React.Fragment>
+										);
+									}
+								)
+							) : (
+								<React.Fragment>
+									<rect
+										x={1}
+										y={0}
+										width={graphWidth - 1}
+										height={graphHeight}
+										className="fill-slate-100"
+									/>
+									<text
+										x={graphWidth / 2}
+										y={graphHeight / 2}
+										textAnchor="middle" // Centers horizontally
+										dominantBaseline="middle" // Centers vertically
+										className="fill-current text-logo text-5xl font-light"
+									>
+										No Data Exists For This Period
+									</text>
+								</React.Fragment>
+							)}
+						</g>
+					</svg>
+				</div>
+				<div
+					className="mt-16"
+					style={{ width: windowSizeInPixels * 0.15 }}
+				>
+					{filteredUserchart.map((user, index) => {
+						const color = colorScale(index.toString());
+						return user.orders.length > 0 ? (
+							<div
+								key={user.userName}
+								className="flex mb-2 cursor-pointer"
+								onClick={(e) => onUsernameClick(user.userName, e)}
+							>
+								<div
+									className="h-6 w-6 rounded-full mr-2"
+									style={{ backgroundColor: color }}
+								></div>
+								<div className="font-light text-neutral-600">
+									{user.userName}
+								</div>
+							</div>
 						) : (
-							<React.Fragment>
-								<rect
-									x={1}
-									y={0}
-									width={graphWidth - 1}
-									height={graphHeight}
-									className="fill-slate-100"
-								/>
-								<text
-									x={graphWidth / 2}
-									y={graphHeight / 2}
-									textAnchor="middle" // Centers horizontally
-									dominantBaseline="middle" // Centers vertically
-									className="fill-current text-logo text-5xl font-light"
-								>
-									No Data Exists For This Period
-								</text>
-							</React.Fragment>
-						)}
-					</g>
-				</svg>
+							<></>
+						);
+					})}
+				</div>
 			</div>
 		</React.Fragment>
 	);
