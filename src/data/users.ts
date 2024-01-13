9e7406715839a16786bd740d0144c5fb4b397c86
francoisdillinger <elmojester@gmail.com>
2024-01-12 22:53:12 -0600

    users dummy data

---
+++ src/data/users.ts
@@ -0,0 +1,513 @@
+export const users = {
+	data: {
+		users: [
+			{
+				userName: "fuckinadmin",
+				orders: [],
+			},
+			{
+				userName: "user1",
+				orders: [
+					{
+						orderId: "2345678901",
+						quantity: 2,
+						orderDate: "2023-07-01",
+						book: {
+							bookTitle: "1984",
+						},
+					},
+					{
+						orderId: "2345678901",
+						quantity: 1,
+						orderDate: "2023-07-01",
+						book: {
+							bookTitle: "To the Lighthouse",
+						},
+					},
+					{
+						orderId: "2345678901",
+						quantity: 3,
+						orderDate: "2023-07-01",
+						book: {
+							bookTitle: "The Old Man and The Sea",
+						},
+					},
+					{
+						orderId: "9274384012",
+						quantity: 6,
+						orderDate: "2023-04-15",
+						book: {
+							bookTitle: "1984",
+						},
+					},
+					{
+						orderId: "8973451026",
+						quantity: 3,
+						orderDate: "2023-05-03",
+						book: {
+							bookTitle: "Mrs Dalloway",
+						},
+					},
+					{
+						orderId: "6351097238",
+						quantity: 1,
+						orderDate: "2023-08-11",
+						book: {
+							bookTitle: "Ulysses",
+						},
+					},
+					{
+						orderId: "1349056172",
+						quantity: 7,
+						orderDate: "2023-03-30",
+						book: {
+							bookTitle: "Pride and Prejudice",
+						},
+					},
+					{
+						orderId: "1049372068",
+						quantity: 8,
+						orderDate: "2023-05-02",
+						book: {
+							bookTitle: "Oliver Twist",
+						},
+					},
+					{
+						orderId: "7032841956",
+						quantity: 4,
+						orderDate: "2023-03-27",
+						book: {
+							bookTitle: "To Kill a Mockingbird",
+						},
+					},
+					{
+						orderId: "1203975408",
+						quantity: 6,
+						orderDate: "2023-12-24",
+						book: {
+							bookTitle: "Nine Stories",
+						},
+					},
+					{
+						orderId: "4092175368",
+						quantity: 5,
+						orderDate: "2023-09-09",
+						book: {
+							bookTitle: "Animal Farm",
+						},
+					},
+					{
+						orderId: "3901247569",
+						quantity: 7,
+						orderDate: "2023-06-19",
+						book: {
+							bookTitle: "The Old Man and The Sea",
+						},
+					},
+					{
+						orderId: "8012437561",
+						quantity: 4,
+						orderDate: "2023-03-18",
+						book: {
+							bookTitle: "Finnegans Wake",
+						},
+					},
+					{
+						orderId: "8031247561",
+						quantity: 7,
+						orderDate: "2022-12-18",
+						book: {
+							bookTitle: "Emma",
+						},
+					},
+					{
+						orderId: "9031247560",
+						quantity: 2,
+						orderDate: "2023-09-15",
+						book: {
+							bookTitle: "Adventures of Huckleberry Finn",
+						},
+					},
+					{
+						orderId: "9031245061",
+						quantity: 8,
+						orderDate: "2023-06-11",
+						book: {
+							bookTitle: "Go Set a Watchman",
+						},
+					},
+					{
+						orderId: "9031245067",
+						quantity: 2,
+						orderDate: "2023-03-04",
+						book: {
+							bookTitle: "Franny and Zooey",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 2,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "Pride and Prejudice",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 7,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "The Adventures of Tom Sawyer",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 5,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "1984",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 9,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "Ulysses",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 3,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "Oliver Twist",
+						},
+					},
+				],
+			},
+			{
+				userName: "user2",
+				orders: [
+					{
+						orderId: "4567890123",
+						quantity: 1,
+						orderDate: "2023-07-07",
+						book: {
+							bookTitle: "Ulysses",
+						},
+					},
+					{
+						orderId: "4567890123",
+						quantity: 2,
+						orderDate: "2023-07-07",
+						book: {
+							bookTitle: "Pride and Prejudice",
+						},
+					},
+					{
+						orderId: "4567890123",
+						quantity: 2,
+						orderDate: "2023-07-07",
+						book: {
+							bookTitle: "Oliver Twist",
+						},
+					},
+					{
+						orderId: "4567890123",
+						quantity: 1,
+						orderDate: "2023-07-07",
+						book: {
+							bookTitle: "Adventures of Huckleberry Finn",
+						},
+					},
+					{
+						orderId: "3648927165",
+						quantity: 8,
+						orderDate: "2023-02-25",
+						book: {
+							bookTitle: "Animal Farm",
+						},
+					},
+					{
+						orderId: "4728395102",
+						quantity: 9,
+						orderDate: "2023-07-12",
+						book: {
+							bookTitle: "The Old Man and The Sea",
+						},
+					},
+					{
+						orderId: "5902371648",
+						quantity: 4,
+						orderDate: "2023-09-07",
+						book: {
+							bookTitle: "Finnegans Wake",
+						},
+					},
+					{
+						orderId: "7208391564",
+						quantity: 6,
+						orderDate: "2022-11-04",
+						book: {
+							bookTitle: "Emma",
+						},
+					},
+					{
+						orderId: "1902735461",
+						quantity: 2,
+						orderDate: "2023-06-19",
+						book: {
+							bookTitle: "Adventures of Huckleberry Finn",
+						},
+					},
+					{
+						orderId: "8734092156",
+						quantity: 3,
+						orderDate: "2023-02-12",
+						book: {
+							bookTitle: "Go Set a Watchman",
+						},
+					},
+					{
+						orderId: "3840921507",
+						quantity: 1,
+						orderDate: "2022-11-16",
+						book: {
+							bookTitle: "Franny and Zooey",
+						},
+					},
+					{
+						orderId: "9012354761",
+						quantity: 2,
+						orderDate: "2023-08-12",
+						book: {
+							bookTitle: "To the Lighthouse",
+						},
+					},
+					{
+						orderId: "8901234756",
+						quantity: 6,
+						orderDate: "2023-05-17",
+						book: {
+							bookTitle: "A Farewell to Arms",
+						},
+					},
+					{
+						orderId: "1023475619",
+						quantity: 5,
+						orderDate: "2023-02-12",
+						book: {
+							bookTitle: "Wuthering Heights",
+						},
+					},
+					{
+						orderId: "1023475610",
+						quantity: 1,
+						orderDate: "2023-11-14",
+						book: {
+							bookTitle: "Great Expectations",
+						},
+					},
+					{
+						orderId: "8012347501",
+						quantity: 5,
+						orderDate: "2023-08-10",
+						book: {
+							bookTitle: "The Adventures of Tom Sawyer",
+						},
+					},
+					{
+						orderId: "8031247560",
+						quantity: 4,
+						orderDate: "2023-05-09",
+						book: {
+							bookTitle: "The Catcher in the Rye",
+						},
+					},
+				],
+			},
+			{
+				userName: "user3",
+				orders: [
+					{
+						orderId: "9012345678",
+						quantity: 3,
+						orderDate: "2023-07-10",
+						book: {
+							bookTitle: "To Kill a Mockingbird",
+						},
+					},
+					{
+						orderId: "9012345678",
+						quantity: 1,
+						orderDate: "2023-07-10",
+						book: {
+							bookTitle: "Nine Stories",
+						},
+					},
+					{
+						orderId: "9012345678",
+						quantity: 2,
+						orderDate: "2023-07-10",
+						book: {
+							bookTitle: "Franny and Zooey",
+						},
+					},
+					{
+						orderId: "6924835071",
+						quantity: 2,
+						orderDate: "2023-06-18",
+						book: {
+							bookTitle: "To the Lighthouse",
+						},
+					},
+					{
+						orderId: "1289073451",
+						quantity: 5,
+						orderDate: "2023-01-23",
+						book: {
+							bookTitle: "A Farewell to Arms",
+						},
+					},
+					{
+						orderId: "7341905627",
+						quantity: 3,
+						orderDate: "2022-12-13",
+						book: {
+							bookTitle: "Wuthering Heights",
+						},
+					},
+					{
+						orderId: "8490173256",
+						quantity: 5,
+						orderDate: "2023-04-06",
+						book: {
+							bookTitle: "Great Expectations",
+						},
+					},
+					{
+						orderId: "2903874156",
+						quantity: 10,
+						orderDate: "2023-07-14",
+						book: {
+							bookTitle: "The Adventures of Tom Sawyer",
+						},
+					},
+					{
+						orderId: "2938471056",
+						quantity: 7,
+						orderDate: "2023-01-28",
+						book: {
+							bookTitle: "The Catcher in the Rye",
+						},
+					},
+					{
+						orderId: "8309127546",
+						quantity: 9,
+						orderDate: "2022-10-15",
+						book: {
+							bookTitle: "1984",
+						},
+					},
+					{
+						orderId: "2091354768",
+						quantity: 8,
+						orderDate: "2023-07-13",
+						book: {
+							bookTitle: "Mrs Dalloway",
+						},
+					},
+					{
+						orderId: "9012347569",
+						quantity: 3,
+						orderDate: "2023-04-13",
+						book: {
+							bookTitle: "Ulysses",
+						},
+					},
+					{
+						orderId: "9031247561",
+						quantity: 2,
+						orderDate: "2023-01-24",
+						book: {
+							bookTitle: "Pride and Prejudice",
+						},
+					},
+					{
+						orderId: "9031247506",
+						quantity: 9,
+						orderDate: "2023-10-17",
+						book: {
+							bookTitle: "Oliver Twist",
+						},
+					},
+					{
+						orderId: "1023475061",
+						quantity: 3,
+						orderDate: "2023-07-14",
+						book: {
+							bookTitle: "To Kill a Mockingbird",
+						},
+					},
+					{
+						orderId: "1023450612",
+						quantity: 10,
+						orderDate: "2023-04-06",
+						book: {
+							bookTitle: "Nine Stories",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 2,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "A Farewell to Arms",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 4,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "Great Expectations",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 3,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "Finnegans Wake",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 5,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "To Kill a Mockingbird",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 7,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "Nine Stories",
+						},
+					},
+					{
+						orderId: "1234567890",
+						quantity: 1,
+						orderDate: "2023-07-23",
+						book: {
+							bookTitle: "Animal Farm",
+						},
+					},
+				],
+			},
+		],
+	},
+};
