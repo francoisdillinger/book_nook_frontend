e937047c01c6af1a84be52e0c261f707bbfae3e6
francoisdillinger <elmojester@gmail.com>
2024-01-14 23:47:13 -0600

    Refactor UsersAdminChart component and extract utility functions

--- src/components/UsersAdminChart.tsx
+++ src/components/UsersAdminChart.tsx
@@ -5,162 +5,23 @@ import { users, UsersType } from "../data/users";
 import XAxis from "./XAxis";
 import YAxis from "./YAxis";
 import ReactSelect from "./ReactSelect";
-
-type UserType = {
-	userName: string;
-	orders: {
-		orderId: string;
-		quantity: number;
-		orderDate: string; // in 'YYYY-MM-DD' format
-		book: {
-			bookTitle: string;
-		};
-	}[];
-};
-
-type ProcessedOrder = {
-	date: string;
-	quantity: number;
-	orderId: string;
-};
-
-type ProcessedUserType = {
-	userName: string;
-	orders: ProcessedOrder[];
-};
-
-const reformatUserData = (users: UsersType): UserType[] => {
-	return users.data.users.map((user) => user);
-};
-
-const processDataForLineChart = (users: UserType[]): ProcessedUserType[] => {
-	return users.map((user) => {
-		const aggregationByDateAndId: Record<string, ProcessedOrder> = {};
-
-		user.orders.forEach((order) => {
-			const combinedKey = `${order.orderDate}-${order.orderId}`;
-
-			if (aggregationByDateAndId[combinedKey]) {
-				aggregationByDateAndId[combinedKey].quantity += order.quantity;
-			} else {
-				aggregationByDateAndId[combinedKey] = {
-					date: order.orderDate, // Keeping date as string
-					quantity: order.quantity,
-					orderId: order.orderId,
-				};
-			}
-		});
-
-		// Sort by date string. Assumes date strings are in a format that allows lexicographical sorting (like 'YYYY-MM-DD')
-		const aggregatedOrders = Object.values(aggregationByDateAndId).sort(
-			(a, b) => new Date(a.date).getTime() - new Date(b.date).getTime()
-		);
-
-		return {
-			userName: user.userName,
-			orders: aggregatedOrders,
-		};
-	});
-};
-
-const sixMonthsAgo = (date: string) => {
-	let pastDate = new Date(date);
-	pastDate.setMonth(pastDate.getMonth() - 6);
-	return pastDate;
-};
-
-const getFilteredData = (
-	filter: string,
-	userData: UserType[]
-): ProcessedUserType[] => {
-	const now = new Date();
-
-	switch (filter) {
-		case "day":
-			return processDataForLineChart(
-				userData.map((user) => {
-					return {
-						...user,
-						orders: user.orders.filter(
-							(order) => new Date(order.orderDate) >= d3.timeDay.offset(now, -1)
-						),
-					};
-				})
-			);
-			break;
-		case "week":
-			return processDataForLineChart(
-				userData.map((user) => {
-					return {
-						...user,
-						orders: user.orders.filter(
-							(order) =>
-								new Date(order.orderDate) >= d3.timeWeek.offset(now, -1)
-						),
-					};
-				})
-			);
-			break;
-		case "month":
-			return processDataForLineChart(
-				userData.map((user) => {
-					return {
-						...user,
-						orders: user.orders.filter(
-							(order) =>
-								new Date(order.orderDate) >= d3.timeMonth.offset(now, -1)
-						),
-					};
-				})
-			);
-			break;
-		case "half-year":
-			return processDataForLineChart(
-				userData.map((user) => {
-					return {
-						...user,
-						orders: user.orders.filter(
-							(order) =>
-								new Date(order.orderDate) >= sixMonthsAgo(now.toISOString())
-						),
-					};
-				})
-			);
-			break;
-		case "year":
-			return processDataForLineChart(
-				userData.map((user) => {
-					return {
-						...user,
-						orders: user.orders.filter(
-							(order) =>
-								new Date(order.orderDate) >= d3.timeYear.offset(now, -1)
-						),
-					};
-				})
-			);
-			break;
-		case "max":
-		default:
-			return processDataForLineChart(userData);
-	}
-};
+import {
+	reformatUserData,
+	getFilteredData,
+} from "../utils/usersAdminChartUtilities";
 
 export default function UsersAdminChart() {
 	const [focusedUser, setFocusedUser] = useState("");
-	// console.log(focusedUser);
 	const [windowSizeInPixels, setWindowSizeInPixels] = useState(
 		window.innerWidth
 	);
 	const [timeFilter, setTimeFilter] = useState("max");
 	const reformatedUserData = reformatUserData(users);
 	const filteredUserchart = getFilteredData(timeFilter, reformatedUserData);
-	// const hasData = filteredUserchart.map((user) => user.orders.length);
 	const hasData = filteredUserchart.reduce(
 		(accumulator, users) => accumulator + users.orders.length,
 		0
 	);
-	// console.log(filteredUserchart);
 	useEffect(() => {
 		const windowSizePixels = () => {
 			setWindowSizeInPixels(window.innerWidth);
@@ -213,12 +74,6 @@ export default function UsersAdminChart() {
 		dateExtent[0] && dateExtent[1] ? dateExtent : [new Date(), new Date()];
 
 	const x = d3.scaleTime().domain(domain).range([0, graphWidth]);
-
-	// Set the domain of the x scale using the parsed dates
-	// const x = d3
-	// 	.scaleTime()
-	// 	.domain(d3.extent(parsedDates))
-	// 	.range([0, graphWidth]);
 
 	const y = d3
 		.scaleLinear()
