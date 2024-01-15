6eb3b0b049be7564fda97c9a296f6a2b4633899e
francoisdillinger <elmojester@gmail.com>
2024-01-15 01:59:29 -0600

    Add ChartToolTip component

---
+++ src/components/ChartToolTip.tsx
@@ -0,0 +1,25 @@
+export type TooltipStateType = {
+	visible: boolean;
+	content: JSX.Element | null;
+	x: number;
+	y: number;
+};
+
+export default function ChartToolTip({
+	tooltip,
+}: {
+	tooltip: TooltipStateType;
+}) {
+	return (
+		<div
+			className="absolute bg-white rounded-md p-2 text-slate-500 font-light text-xs shadow-2xl"
+			style={{
+				display: tooltip.visible ? "block" : "none",
+				left: `${tooltip.x}px`,
+				top: `${tooltip.y}px`,
+			}}
+		>
+			{tooltip.content}
+		</div>
+	);
+}
