4403129eca52e32121d0ec11b9179674e6a30bc7
francoisdillinger <elmojester@gmail.com>
2024-01-17 03:07:41 -0600

    Update user authentication logic

--- src/components/UsersAdminPieChart.tsx
+++ src/components/UsersAdminPieChart.tsx
@@ -74,7 +74,7 @@ const UsersAdminPieChart = ({
 			)
 		);
 		setReducedUsersData(reducedData);
-	}, [graphWidth, timeFilter]);
+	}, [timeFilter]);
 
 	const pie = useMemo(() => {
 		return d3
@@ -87,7 +87,7 @@ const UsersAdminPieChart = ({
 		const smallerSide = Math.min(graphWidth, graphHeight);
 		return (smallerSide / 2) * 0.8; // 80% of half the smaller dimension
 	}, [graphWidth, graphHeight]);
-	// const toRadians = (angleInDegrees) => angleInDegrees * (Math.PI / 180);
+
 	const arcPath = useMemo(() => {
 		return d3
 			.arc()
@@ -95,137 +95,212 @@ const UsersAdminPieChart = ({
 			.innerRadius(radius / 2);
 	}, [radius, reducedUsersData]);
 
-	const generateArcPaths = (startAngleDegrees, endAngleDegrees) => {
-		// const startAngleRadians = toRadians(startAngleDegrees);
-		// const endAngleRadians = toRadians(endAngleDegrees);
-
-		// Start path (at the start angle)
-		const startPath = arcPath
-			.startAngle(startAngleDegrees)
-			.endAngle(startAngleDegrees)(); // Empty call to generate path
-
-		// End path (at the end angle)
-		const endPath = arcPath
-			.startAngle(startAngleDegrees)
-			.endAngle(endAngleDegrees)(); // Empty call to generate path
-
-		return { startPath, endPath };
+	const handleMouseEnter = (e, d) => {
+		console.log("We in here");
+		const content = (
+			<div>
+				<div>
+					<span className="text-slate-600 font-bold">Username:</span>{" "}
+					{d.data.userName}
+				</div>
+				<div>
+					<span className="text-slate-600 font-bold">Order Quantity:</span>{" "}
+					{d.data.totalBooksOrdered.toString()}
+				</div>
+			</div>
+		);
+		setTooltip({
+			visible: true,
+			content: content,
+			x: e.clientX,
+			y: e.clientY,
+		});
+	};
+	const handleMouseLeave = () => {
+		setTooltip({ ...tooltip, visible: false });
 	};
 
-	// Example usage
-	// const { startPath, endPath } = generateArcPaths(0, 90); // 0 to 90 degrees
-	// console.log("Start Path:", startPath);
-	// console.log("End Path:", endPath);
+	const ref = useRef();
+
+	useEffect(() => {
+		if (ref.current && reducedUsersData) {
+			// Check if reducedUsersData is not undefined
+			const paths = d3
+				.select(ref.current)
+				.selectAll("path")
+				.data(pie(reducedUsersData));
+
+			paths
+				.enter()
+				.append("path")
+				.merge(paths)
+				.attr("d", arcPath)
+				.attr("fill", (d, i) =>
+					focusedUser === d.data.userName || focusedUser === ""
+						? colorScale(i.toString())
+						: "gray"
+				)
+				.attr("opacity", (d) =>
+					focusedUser === d.data.userName || focusedUser === "" ? 1 : 0.2
+				)
+
+				.on("mouseenter", handleMouseEnter)
+				.on("mouseleave", handleMouseLeave)
+				.transition()
+				.duration(1000)
+				.ease(d3.easeBounceOut)
+				.attrTween("d", (d) => {
+					const interpolate = d3.interpolate(d.startAngle, d.endAngle);
+					return (t) => arcPath({ ...d, endAngle: interpolate(t) });
+				});
+		}
+	}, [reducedUsersData, pie, arcPath, colorScale]);
+
+	useEffect(() => {
+		if (ref.current && reducedUsersData) {
+			// Check if reducedUsersData is not undefined
+			const paths = d3
+				.select(ref.current)
+				.selectAll("path")
+				.attr("fill", (d, i) =>
+					focusedUser === d.data.userName || focusedUser === ""
+						? colorScale(i.toString())
+						: "gray"
+				)
+				.attr("opacity", (d) =>
+					focusedUser === d.data.userName || focusedUser === "" ? 1 : 0.2
+				);
+		}
+	}, [focusedUser]);
 
 	return (
-		<React.Fragment>
-			<g transform={`translate(${graphWidth / 2}, ${graphHeight / 2})`}>
+		<g
+			ref={ref}
+			transform={`translate(${graphWidth / 2}, ${graphHeight / 2})`}
+		>
+			<text
+				x={0}
+				y={-graphHeight + 200}
+				textAnchor="middle"
+				className="fill-current text-neutral-500 text-2xl"
+			>
+				Total Percentage of User Book Orders
+			</text>
+			<text
+				x={0}
+				y={0}
+				textAnchor="middle"
+				dominantBaseline="middle"
+				className="fill-current text-neutral-600 text-5xl font-light"
+			>
 				{" "}
-				<text
-					x={0}
-					y={-graphHeight + 200}
-					textAnchor="middle"
-					className="fill-current text-neutral-500 text-2xl"
-				>
-					Total Percentage of User Book Orders
-				</text>
-				<text
-					x={0}
-					y={0}
-					textAnchor="middle"
-					dominantBaseline="middle"
-					className="fill-current text-neutral-600 text-5xl font-light"
-				>
-					{focusedUser != ""
-						? parseFloat(
-								(
-									((reducedUsersData?.find(
-										(user) => user.userName === focusedUser
-									)?.totalBooksOrdered || 0) /
-										totalOrderCount) *
-									100
-								).toString()
-						  ).toFixed(0)
-						: "100"}
-					%
-				</text>
-				{hasData &&
-					pie(reducedUsersData)?.map((user, index) => {
-						const color = colorScale(index.toString());
-						console.log(user.startAngle);
-						// const arcTween = (d) => {
-						// 	var i = d3.interpolate(user.endAngle, user.startAngle);
-						// 	return function (t) {
-						// 		user.startAngle = i(t);
-						// 		return arcPath(d);
-						// 	};
-						// };
-						const { startPath, endPath } = generateArcPaths(
-							user.startAngle,
-							user.endAngle
-						); // 0 to 90 degrees
-						console.log("Start Path:", startPath);
-						console.log("End Path:", endPath);
-						return (
-							<motion.path
-								key={user.data.userName}
-								d={arcPath(user)}
-								initial={{ pathLength: 0 }}
-								animate={{ pathLength: 1 }}
-								transition={{ duration: 0.5, ease: "easeInOut" }}
-								strokeWidth={60}
-								stroke={color}
-								// transition={{
-								// 	duration: 0.5,
-								// 	ease: [0.17, 0.67, 0.83, 0.67], // Bezier curve for a bounce effect
-								// 	type: "spring", // Use spring physics for bounce
-								// 	damping: 10, // Adjust damping for more or less bounce
-								// 	stiffness: 100, // Adjust stiffness for more or less bounce
-								// }}
-								fill="transparent"
-								// fill={
-								// 	focusedUser === user.data.userName || focusedUser === ""
-								// 		? color
-								// 		: "gray"
-								// }
-								opacity={
-									focusedUser === user.data.userName || focusedUser === ""
-										? 1
-										: 0.2
-								}
-								onMouseEnter={(e) => {
-									const content = (
-										<div>
-											<div>
-												<span className="text-slate-600 font-bold">
-													Username:
-												</span>{" "}
-												{user.data.userName}
-											</div>
-											<div>
-												<span className="text-slate-600 font-bold">
-													Order Quantity:
-												</span>{" "}
-												{user.data.totalBooksOrdered.toString()}
-											</div>
-										</div>
-									);
-									setTooltip({
-										visible: true,
-										content: content,
-										x: e.clientX,
-										y: e.clientY,
-									});
-								}}
-								onMouseLeave={() => {
-									setTooltip({ ...tooltip, visible: false });
-								}}
-							></motion.path>
-						);
-					})}
-			</g>
-		</React.Fragment>
+				{focusedUser != ""
+					? parseFloat(
+							(
+								((reducedUsersData?.find(
+									(user) => user.userName === focusedUser
+								)?.totalBooksOrdered || 0) /
+									totalOrderCount) *
+								100
+							).toString()
+					  ).toFixed(0)
+					: "100"}
+				%
+			</text>
+		</g>
 	);
 };
 
 export default UsersAdminPieChart;
+
+// return (
+//     <React.Fragment>
+//         <g transform={`translate(${graphWidth / 2}, ${graphHeight / 2})`}>
+//             {" "}
+//             <text
+//                 x={0}
+//                 y={-graphHeight + 200}
+//                 textAnchor="middle"
+//                 className="fill-current text-neutral-500 text-2xl"
+//             >
+//                 Total Percentage of User Book Orders
+//             </text>
+//             <text
+//                 x={0}
+//                 y={0}
+//                 textAnchor="middle"
+//                 dominantBaseline="middle"
+//                 className="fill-current text-neutral-600 text-5xl font-light"
+//             >
+//                 {focusedUser != ""
+//                     ? parseFloat(
+//                             (
+//                                 ((reducedUsersData?.find(
+//                                     (user) => user.userName === focusedUser
+//                                 )?.totalBooksOrdered || 0) /
+//                                     totalOrderCount) *
+//                                 100
+//                             ).toString()
+//                       ).toFixed(0)
+//                     : "100"}
+//                 %
+//             </text>
+//             {hasData &&
+//                 pie(reducedUsersData).map((user, index) => {
+//                     const color = colorScale(index.toString());
+
+//                     return (
+//                         <motion.path
+//                             key={user.data.userName}
+//                             // d={arcPath(user)}
+//                             // transform={"rotate(0,45)"}
+//                             // transition={{
+//                             // 	duration: 1,
+//                             // 	ease: [0.17, 0.67, 0.83, 0.67], // Bezier curve for a bounce effect
+//                             // 	type: "spring", // Use spring physics for bounce
+//                             // 	damping: 10, // Adjust damping for more or less bounce
+//                             // 	stiffness: 100, // Adjust stiffness for more or less bounce
+//                             // }}
+//                             fill={
+//                                 focusedUser === user.data.userName || focusedUser === ""
+//                                     ? color
+//                                     : "gray"
+//                             }
+//                             opacity={
+//                                 focusedUser === user.data.userName || focusedUser === ""
+//                                     ? 1
+//                                     : 0.2
+//                             }
+//                             onMouseEnter={(e) => {
+//                                 const content = (
+//                                     <div>
+//                                         <div>
+//                                             <span className="text-slate-600 font-bold">
+//                                                 Username:
+//                                             </span>{" "}
+//                                             {user.data.userName}
+//                                         </div>
+//                                         <div>
+//                                             <span className="text-slate-600 font-bold">
+//                                                 Order Quantity:
+//                                             </span>{" "}
+//                                             {user.data.totalBooksOrdered.toString()}
+//                                         </div>
+//                                     </div>
+//                                 );
+//                                 setTooltip({
+//                                     visible: true,
+//                                     content: content,
+//                                     x: e.clientX,
+//                                     y: e.clientY,
+//                                 });
+//                             }}
+//                             onMouseLeave={() => {
+//                                 setTooltip({ ...tooltip, visible: false });
+//                             }}
+//                         ></motion.path>
+//                     );
+//                 })}
+//         </g>
+//     </React.Fragment>
+// );
