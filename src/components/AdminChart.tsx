5f78be8ffafffabcaade802f76eb3c6cf3c0d721
francoisdillinger <elmojester@gmail.com>
2024-01-18 23:27:16 -0600

    Update color scale in AdminChart component

--- src/components/AdminChart.tsx
+++ src/components/AdminChart.tsx
@@ -8,6 +8,7 @@ import { TooltipStateType } from "./ChartToolTip";
 import UsersAdminChart from "./UsersAdminChart";
 import UsersAdminBarChart from "./UsersAdminBarChart";
 import UsersAdminPieChart from "./UsersAdminPieChart";
+import ResponsiveSVGContainer from "./ResponsiveSVGContainer";
 
 export type MarginType = {
 	top: number;
@@ -19,7 +20,7 @@ export type MarginType = {
 const margin: MarginType = { top: 20, right: 20, bottom: 50, left: 80 };
 // const svgHeight = 450;
 // const graphLineChartHeight = svgHeight - margin.top - margin.bottom;
-const colorScale = d3.scaleOrdinal(d3.schemeTableau10);
+const colorScale = d3.scaleOrdinal(d3.schemeSet3);
 
 export default function AdminChart() {
 	// const svgLineChartRef = useRef<SVGSVGElement>(null);
@@ -61,17 +62,20 @@ export default function AdminChart() {
 
 	return (
 		<React.Fragment>
+			{/* <div className="flex lg:ml-20 xl:ml-28">
+
+			</div> */}
 			<div className="flex lg:ml-20 xl:ml-28">
 				<div
-					className="bg-white rounded-lg my-2 pt-2"
-					style={{ width: windowSizeInPixels * 0.9 }}
+					className="bg-white rounded-lg my-2 pt-2 w-full"
+					// style={{ width: windowSizeInPixels * 0.9 }}
 				>
-					<div className="h-12 flex items-center justify-between">
+					<div className="h-12 flex flex-wrap  md:flex-nowrap md:items-center md:justify-between">
 						<ChartTimePeriodButtons
 							timeFilter={timeFilter}
 							setTimeFilter={setTimeFilter}
 						/>
-						<div className=" mr-5 pt-2">
+						<div className="ml-20 mt-8 md:mr-5 md:mt-0 md:pt-2">
 							{selectOptions ? (
 								<ReactSelect
 									options={selectOptions}
@@ -84,14 +88,46 @@ export default function AdminChart() {
 							)}
 						</div>
 					</div>
+
 					<ChartToolTip tooltip={tooltip} />
-					<div className="">
-						<UsersAdminChart
+
+					<div className=" flex justify-center mt-10 md:mt-0">
+						<div className="w-full h-[450px]">
+							<ResponsiveSVGContainer>
+								<UsersAdminChart
+									margin={margin}
+									timeFilter={timeFilter}
+									// windowSizeInPixels={windowSizeInPixels}
+									// graphHeight={graphLineChartHeight}
+									// graphWidth={graphLineChartWidth}
+									tooltip={tooltip}
+									setTooltip={setTooltip}
+									users={users}
+									colorScale={colorScale}
+									hasData={hasData}
+									setHasData={setHasData}
+									setSelectOptions={setSelectOptions}
+									focusedUser={focusedUser}
+								/>
+							</ResponsiveSVGContainer>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div
+				className="border-box flex flex-wrap lg:flex-nowrap lg:ml-20 xl:ml-28 justify-between md:gap-4"
+				// style={{ width: windowSizeInPixels * 0.9 }}
+			>
+				<div className="bg-white rounded-lg my-2 pt-2 w-full lg:w-3/4 h-96">
+					<ResponsiveSVGContainer>
+						<UsersAdminBarChart
 							margin={margin}
 							timeFilter={timeFilter}
-							windowSizeInPixels={windowSizeInPixels}
-							// graphHeight={graphLineChartHeight}
-							// graphWidth={graphLineChartWidth}
+							// windowSizeInPixels={windowSizeInPixels}
+							// graphHeight={350 - 10 - 50}
+							// graphWidth={
+							// 	windowSizeInPixels * 0.54 - margin.left - margin.right
+							// }
 							tooltip={tooltip}
 							setTooltip={setTooltip}
 							users={users}
@@ -101,45 +137,25 @@ export default function AdminChart() {
 							setSelectOptions={setSelectOptions}
 							focusedUser={focusedUser}
 						/>
-					</div>
+					</ResponsiveSVGContainer>
 				</div>
-			</div>
-			<div
-				className="flex lg:ml-20 xl:ml-28 justify-between"
-				style={{ width: windowSizeInPixels * 0.9 }}
-			>
-				<div className="bg-white rounded-lg my-2 pt-2">
-					<UsersAdminBarChart
-						margin={margin}
-						timeFilter={timeFilter}
-						windowSizeInPixels={windowSizeInPixels}
-						graphHeight={350 - 10 - 50}
-						graphWidth={windowSizeInPixels * 0.54 - margin.left - margin.right}
-						tooltip={tooltip}
-						setTooltip={setTooltip}
-						users={users}
-						colorScale={colorScale}
-						hasData={hasData}
-						setHasData={setHasData}
-						setSelectOptions={setSelectOptions}
-						focusedUser={focusedUser}
-					/>
-				</div>
-				<div className="bg-white rounded-lg my-2 pt-2">
-					<UsersAdminPieChart
-						timeFilter={timeFilter}
-						windowSizeInPixels={windowSizeInPixels}
-						graphWidth={windowSizeInPixels * 0.34}
-						graphHeight={350}
-						tooltip={tooltip}
-						setTooltip={setTooltip}
-						users={users}
-						colorScale={colorScale}
-						hasData={hasData}
-						setHasData={setHasData}
-						setSelectOptions={setSelectOptions}
-						focusedUser={focusedUser}
-					/>
+				<div className="bg-white rounded-lg my-2 pt-2 w-full  lg:w-1/4 h-96">
+					<ResponsiveSVGContainer>
+						<UsersAdminPieChart
+							timeFilter={timeFilter}
+							// windowSizeInPixels={windowSizeInPixels}
+							// graphWidth={windowSizeInPixels * 0.34}
+							// graphHeight={350}
+							tooltip={tooltip}
+							setTooltip={setTooltip}
+							users={users}
+							colorScale={colorScale}
+							hasData={hasData}
+							setHasData={setHasData}
+							setSelectOptions={setSelectOptions}
+							focusedUser={focusedUser}
+						/>
+					</ResponsiveSVGContainer>
 				</div>
 			</div>
 		</React.Fragment>
