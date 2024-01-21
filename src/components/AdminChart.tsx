2826c52b4cd09e84120a838d05fe53e6c830c7fc
francoisdillinger <elmojester@gmail.com>
2024-01-21 04:37:51 -0600

    Refactor AdminChart component with housekeeping

--- src/components/AdminChart.tsx
+++ src/components/AdminChart.tsx
@@ -18,17 +18,10 @@ export type MarginType = {
 };
 
 const margin: MarginType = { top: 20, right: 20, bottom: 50, left: 80 };
-// const svgHeight = 450;
-// const graphLineChartHeight = svgHeight - margin.top - margin.bottom;
 const colorScale = d3.scaleOrdinal(d3.schemeSet3);
 
 export default function AdminChart() {
-	// const svgLineChartRef = useRef<SVGSVGElement>(null);
-	// const graphLineChartRef = useRef<SVGSVGElement>(null);
 	const [focusedUser, setFocusedUser] = useState("");
-	// const [windowSizeInPixels, setWindowSizeInPixels] = useState(
-	// 	window.innerWidth
-	// );
 	const [timeFilter, setTimeFilter] = useState("max");
 	const [hasData, setHasData] = useState(0);
 	const [selectOptions, setSelectOptions] = useState("");
@@ -38,27 +31,17 @@ export default function AdminChart() {
 		x: 0,
 		y: 0,
 	});
-	// const graphLineChartWidth =
-	// 	windowSizeInPixels <= 800
-	// 		? windowSizeInPixels - margin.left - margin.right
-	// 		: windowSizeInPixels * 0.9 - margin.left - margin.right;
 
-	// useEffect(() => {
-	// 	const windowSizePixels = () => {
-	// 		setWindowSizeInPixels(window.innerWidth);
-	// 		console.log(window.innerWidth);
-	// 	};
-	// 	const onGlobalClick = () => {
-	// 		setFocusedUser("");
-	// 	};
-	// 	window.addEventListener("resize", windowSizePixels);
-	// 	window.addEventListener("click", onGlobalClick);
+	useEffect(() => {
+		const onGlobalClick = () => {
+			setFocusedUser("");
+		};
+		window.addEventListener("click", onGlobalClick);
 
-	// 	return () => {
-	// 		window.removeEventListener("resize", windowSizePixels);
-	// 		window.removeEventListener("click", onGlobalClick);
-	// 	};
-	// }, []);
+		return () => {
+			window.removeEventListener("click", onGlobalClick);
+		};
+	}, []);
 
 	return (
 		<React.Fragment>
@@ -253,9 +236,6 @@ export default function AdminChart() {
 								<UsersAdminChart
 									margin={margin}
 									timeFilter={timeFilter}
-									// windowSizeInPixels={windowSizeInPixels}
-									// graphHeight={graphLineChartHeight}
-									// graphWidth={graphLineChartWidth}
 									tooltip={tooltip}
 									setTooltip={setTooltip}
 									users={users}
@@ -270,20 +250,12 @@ export default function AdminChart() {
 					</div>
 				</div>
 			</div>
-			<div
-				className="border-box flex flex-wrap lg:flex-nowrap lg:ml-20 xl:ml-28 justify-between md:gap-4"
-				// style={{ width: windowSizeInPixels * 0.9 }}
-			>
+			<div className="border-box flex flex-wrap lg:flex-nowrap lg:ml-20 xl:ml-28 justify-between md:gap-4">
 				<div className="bg-white rounded-lg my-2 pt-2 w-full lg:w-3/4 h-96">
 					<ResponsiveSVGContainer>
 						<UsersAdminBarChart
 							margin={margin}
 							timeFilter={timeFilter}
-							// windowSizeInPixels={windowSizeInPixels}
-							// graphHeight={350 - 10 - 50}
-							// graphWidth={
-							// 	windowSizeInPixels * 0.54 - margin.left - margin.right
-							// }
 							tooltip={tooltip}
 							setTooltip={setTooltip}
 							users={users}
@@ -299,9 +271,6 @@ export default function AdminChart() {
 					<ResponsiveSVGContainer>
 						<UsersAdminPieChart
 							timeFilter={timeFilter}
-							// windowSizeInPixels={windowSizeInPixels}
-							// graphWidth={windowSizeInPixels * 0.34}
-							// graphHeight={350}
 							tooltip={tooltip}
 							setTooltip={setTooltip}
 							users={users}
