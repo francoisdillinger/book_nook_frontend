47ec200180d98341b585ee3f8ccec2aeee560037
francoisdillinger <elmojester@gmail.com>
2024-01-28 00:07:10 -0600

    Add BarChartXAxis component

---
+++ src/components/ChartComponents/UserChart/BarChartXAxis.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useRef } from "react";
+import { axisBottom } from "d3-axis";
+import { select } from "d3-selection";
+import * as d3 from "d3";
+import { AxisScale } from "d3-axis";
+import { ScaleTime, ScaleBand } from "d3"; // Import ScaleBand from d3-scale
+
+type BarChartXAxisTypes = {
+	xScale: ScaleBand<string>;
+	height: number;
+	ticks: number;
+	width: number;
+};
+
+const BarChartXAxis = ({
+	xScale,
+	height,
+	ticks,
+	width,
+}: BarChartXAxisTypes) => {
+	const ref = useRef<SVGGElement>(null);
+
+	useEffect(() => {
+		const xScaleNumeric = xScale as unknown as AxisScale<number>;
+
+		const xAxis = axisBottom(xScaleNumeric).ticks(ticks);
+
+		const transition = select(ref.current)
+			.transition()
+			.duration(500) // Duration of the transition in milliseconds
+			.ease(d3.easeCubicInOut); // Easing function
+
+		select(ref.current)
+			.call(xAxis as any)
+			.select(".domain")
+			.remove();
+		select(ref.current).selectAll(".tick line").remove();
+		// This will rotate the text which might be needed depending on the number of users
+		select(ref.current)
+			.selectAll("text")
+			.attr("transform", `${width < 700 ? "rotate(-15)" : "rotate(0)"}`)
+			.attr("text-anchor", `${width < 700 ? "end" : "middle"}`);
+	}, [xScale, width]);
+
+	return (
+		<g
+			ref={ref}
+			className="text-neutral-600 "
+			transform={`translate(0,${height})`}
+		/>
+	);
+};
+
+export default BarChartXAxis;
