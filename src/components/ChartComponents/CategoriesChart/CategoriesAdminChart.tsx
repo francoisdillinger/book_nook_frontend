e5f66649e4608be00c4e8e79d6acdf1f324fb63c
francoisdillinger <elmojester@gmail.com>
2024-02-04 16:47:16 -0600

    Refactor CategoriesAdminChart component and add trimCategoriesData function

--- src/components/ChartComponents/CategoriesChart/CategoriesAdminChart.tsx
+++ src/components/ChartComponents/CategoriesChart/CategoriesAdminChart.tsx
@@ -17,10 +17,37 @@ import {
 	totalOrdersReducer,
 	totalsReducer,
 } from "../../../utils/usersAdminChartUtilities";
-import { categories_data } from "../../../data/categories_data";
+import {
+	categories_data,
+	CategoriesDataType,
+} from "../../../data/categories_data";
 import TotalsComponent from "../TotalsComponent";
+
+type TrimmedCategoriesDataType = {
+	categories: {
+		categoryName: string;
+		books: {
+			bookTitle: string;
+			bookOrders: {
+				orderId: string;
+				quantity: number;
+				orderDate: string;
+				orderAmount: number;
+			}[];
+		}[];
+	}[];
+};
+
+const trimCategoriesData = (
+	categories: CategoriesDataType
+): TrimmedCategoriesDataType => {
+	return {
+		categories: categories.data.categories.map((category) => category),
+	};
+};
 
 export default function CategoriesAdminChart() {
 	console.log(categories_data);
+	console.log(trimCategoriesData(categories_data));
 	return <>hi</>;
 }
