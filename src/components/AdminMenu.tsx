7693875d78ad09abd4dea6ee09acb84e01a46e04
francoisdillinger <elmojester@gmail.com>
2024-02-21 16:11:31 -0600

    Update AdminMenu component styling and add BookNook logo and title

--- src/components/AdminMenu.tsx
+++ src/components/AdminMenu.tsx
@@ -19,11 +19,29 @@ export default function AdminMenu() {
 	}, []);
 
 	return (
-		<nav className="bg-gray-100">
+		<nav className="bg-gray-100 ">
 			{/* Do the following if you want to set a max container width, this may be a good idea */}
 			{/* <div className="max-w-7xl mx-auto px-4"> */}
 			<div className=" mx-auto px-4">
-				<div className="flex justify-end pt-2">
+				<div className="flex justify-between pt-2 lg:ml-20 xl:ml-14">
+					<div className="flex items-center">
+						<a
+							href=""
+							className="px-2 py-2"
+						>
+							<img
+								className="h-10 w-10"
+								src="/booknook_logo.svg"
+								alt=""
+							/>
+						</a>
+						<div>
+							<h2 className="font-bold text-xl/4">
+								Book<span className="text-logo">Nook</span>
+							</h2>
+							<p className="text-slate-500 text-xs">Where Stories Live</p>
+						</div>
+					</div>
 					<div className="hidden md:flex justify-end w-40 items-center space-x-1 ">
 						<a
 							href=""
