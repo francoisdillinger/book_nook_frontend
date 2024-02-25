577b3587fe176b5ee93ecf29a555262f3fe15a24
francoisdillinger <elmojester@gmail.com>
2024-02-25 17:22:19 -0600

    Add TableData component to AdminOrders

--- src/components/AdminOrders.tsx
+++ src/components/AdminOrders.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import TableData from "./TableData";
 
 export default function AdminOrders() {
 	return (
@@ -15,30 +16,7 @@ export default function AdminOrders() {
 						</tr>
 					</thead>
 					<tbody className="border-gray-200 border-solid border rounded-lg">
-						<tr>
-							<td>12312312</td>
-							<td>James JEster</td>
-							<td>Ready</td>
-							<td>$45.89</td>
-							<td className="">
-								<div className="border-gray-700 border-solid border rounded-full w-fit m-auto">
-									<svg
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth={1.5}
-										// stroke="currentColor"
-										className="w-4 h-4 stroke-gray-700"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											d="m19.5 8.25-7.5 7.5-7.5-7.5"
-										/>
-									</svg>
-								</div>
-							</td>
-						</tr>
+						<TableData />
 					</tbody>
 				</table>
 			</div>
