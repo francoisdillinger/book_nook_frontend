87303b6eaf7d34db04a1e6bd44cecc8bbc402952
francoisdillinger <elmojester@gmail.com>
2024-02-25 18:18:54 -0600

    Add expand functionality to TableData component

--- src/components/TableData.tsx
+++ src/components/TableData.tsx
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 export default function TableData() {
+	const [expandSlide, setExpandSlide] = useState<boolean>(false);
+
+	const handleExpandSlide = () => {
+		setExpandSlide((prevState) => !prevState);
+	};
+	console.log("Expand: ", expandSlide);
 	return (
 		<React.Fragment>
-			<tr>
+			<tr className="border-gray-200 border-solid border rounded-lg">
 				<td>12312312</td>
 				<td>James JEster</td>
 				<td>Ready</td>
 				<td>$45.89</td>
 				<td className="">
-					<div className="border-gray-700 border-solid border rounded-full w-fit m-auto">
+					<div
+						onClick={handleExpandSlide}
+						className="border-gray-700 border-solid border rounded-full w-fit m-auto"
+					>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
 							viewBox="0 0 24 24"
 							strokeWidth={1.5}
-							// stroke="currentColor"
 							className="w-4 h-4 stroke-gray-700"
 						>
 							<path
@@ -25,6 +34,24 @@ export default function TableData() {
 							/>
 						</svg>
 					</div>
+				</td>
+			</tr>
+			<tr>
+				<td className="">
+					<motion.div
+						initial={{ height: expandSlide ? "0em" : "0em" }}
+						animate={{ height: expandSlide ? "5em" : "0em" }}
+						transition={{
+							duration: 0.1,
+							ease: expandSlide ? [0.17, 0.67] : [0, 0, 0, 0],
+							type: expandSlide ? "spring" : "ease",
+							damping: 8,
+							stiffness: 100,
+						}}
+						className={`relative overflow-hidden `}
+					>
+						hi
+					</motion.div>
 				</td>
 			</tr>
 		</React.Fragment>
