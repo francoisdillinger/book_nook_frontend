e74e4c73cea9b58bc2b24f6263d3c56529990af1
francoisdillinger <elmojester@gmail.com>
2024-02-26 21:51:45 -0600

    Update dropDownDispatch action type

--- src/components/AdminMenu.tsx
+++ src/components/AdminMenu.tsx
@@ -127,7 +127,7 @@ export default function AdminMenu() {
 										<li
 											onClick={() => {
 												setLoggedIn(false);
-												dropDownDispatch({ type: "navigating" });
+												dropDownDispatch({ type: "hasSelectedItem" });
 											}}
 										>
 											<a
