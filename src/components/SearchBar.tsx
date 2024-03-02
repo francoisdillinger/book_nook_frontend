6c39d09a358dfa15ccc0e9e40d4e478f66b188cc
francoisdillinger <elmojester@gmail.com>
2024-03-02 17:09:34 -0600

    Refactor search button styling

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -42,7 +42,7 @@ export default function SearchBar({
 				</div>
 				<button
 					type="submit"
-					className="p-2.5 z-10 text-sm font-medium h-full text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					className={`${buttonStyles}`}
 				>
 					Search
 				</button>
