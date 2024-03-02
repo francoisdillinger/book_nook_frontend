f63f2bc56fba2b26b954ad597cfa859e072d5ac7
francoisdillinger <elmojester@gmail.com>
2024-03-02 17:15:35 -0600

    Fix key warning and update changeHandler in SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -29,13 +29,13 @@ export default function SearchBar({
 					<option>Author</option>
 					<option>Title</option> */}
 					{options.map((option) => (
-						<option>{option}</option>
+						<option key={option}>{option}</option>
 					))}
 				</select>
 
 				<div className="relative w-full">
 					<input
-						onChange={() => changeHandler()}
+						onChange={(event) => changeHandler(event.target.value)}
 						type="search"
 						id="search-dropdown"
 						className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50  border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 focus-visible:outline-none"
