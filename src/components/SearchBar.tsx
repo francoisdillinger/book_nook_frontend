8bed1a77d82ff674d525afb86912088f33b3f9a9
francoisdillinger <elmojester@gmail.com>
2024-03-02 18:18:24 -0600

    Add useState and update event handlers in SearchBar component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 type SearchBarType = {
 	options: string[];
 	placeHolder: string;
 	selectStyles: string;
 	buttonStyles: string;
-	changeHandler: Function;
+	values: { option: string; value: string };
+	optionsHandler: Function;
+	inputHandler: Function;
 	clickHandler: Function;
 };
 
@@ -14,9 +16,28 @@ export default function SearchBar({
 	placeHolder,
 	selectStyles,
 	buttonStyles,
-	changeHandler,
+	values,
+	optionsHandler,
+	inputHandler,
 	clickHandler,
 }: SearchBarType) {
+	// const [inputValue, setInputValue] = useState("");
+	// const [optionsValue, setOptionsValue] = useState(options[0]);
+	// const [searchValues, setSearchValues] = useState({
+	// 	option: options[0],
+	// 	value: "",
+	// });
+	// console.log(optionsValue);
+
+	// const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+	// 	// console.log(event.target.value);
+	// 	setSearchValues({ ...searchValues, value: event.target.value });
+	// };
+
+	// const clickHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
+	// 	// console.log(event);
+	// 	setSearchValues({ ...searchValues, value: "" });
+	// };
 	return (
 		<React.Fragment>
 			<div className="flex min-w-full">
@@ -26,6 +47,7 @@ export default function SearchBar({
 				<select
 					id="book-selector"
 					className={`${selectStyles}`}
+					onChange={(event) => optionsHandler(event)}
 				>
 					{/* <option>All</option>
 					<option>Author</option>
@@ -37,12 +59,13 @@ export default function SearchBar({
 
 				<div className="relative w-full">
 					<input
-						onChange={(event) => changeHandler(event)}
+						onChange={(event) => inputHandler(event)}
 						type="search"
 						id="search-dropdown"
 						className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50  border-l-gray-50 border-l-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-l-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500 focus-visible:outline-none"
 						placeholder={`${placeHolder}`}
 						required
+						value={values.value}
 					/>
 				</div>
 				<button
