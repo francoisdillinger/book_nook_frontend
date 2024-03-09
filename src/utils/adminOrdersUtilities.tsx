f470af2a42390936373ad55aaf9c23f33f4f2bf6
francoisdillinger <elmojester@gmail.com>
2024-03-08 23:30:50 -0600

    Add reformateOrders function to transform orders data structure

--- src/utils/adminOrdersUtilities.tsx
+++ src/utils/adminOrdersUtilities.tsx
@@ -56,3 +56,42 @@ export type OrdersSearchType = {
 export const trimOrders = (orders: OrdersType): TrimmedOrdersType[] => {
 	return [...orders.data.orders];
 };
+
+export const reformateOrders = (
+	orders: TrimmedOrdersType[]
+): ReformatedOrdersType[] => {
+	const orderIds = orders.map((order) => order.orderId);
+	const removedDuplicateIds = [...new Set(orderIds)];
+
+	const newOrders = removedDuplicateIds.map((id) => {
+		const matchingOrders = orders.filter((order) => id === order.orderId);
+		const reformatedOrder = {
+			orderId: matchingOrders[0].orderId,
+			userId: matchingOrders[0].userId,
+			firstName: matchingOrders[0].user.firstName,
+			lastName: matchingOrders[0].user.lastName,
+			orderDate: matchingOrders[0].orderDate,
+			orderStatus: matchingOrders[0].orderStatus,
+			totalAmount: matchingOrders.reduce(
+				(accumulator, order) => accumulator + order.orderAmount,
+				0
+			),
+			totalQuantity: matchingOrders.reduce(
+				(accumulator, order) => accumulator + order.quantity,
+				0
+			),
+			orders: matchingOrders.map((order) => {
+				return {
+					bookId: order.bookId,
+					bookTitle: order.book.bookTitle,
+					isbn: order.book.isbn,
+					orderAmount: order.orderAmount,
+					quantity: order.quantity,
+				};
+			}),
+		};
+		return reformatedOrder;
+	});
+	// console.log("Orders: ", newOrders);
+	return newOrders;
+};
