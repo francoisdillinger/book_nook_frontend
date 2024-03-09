2917a8f5f3b7a91d8795274899899986139d2fc5
francoisdillinger <elmojester@gmail.com>
2024-03-09 16:56:56 -0600

    Refactor OrderedItem component to display book details and quantity

--- src/components/OrderedItem.tsx
+++ src/components/OrderedItem.tsx
@@ -1,14 +1,41 @@
 import React from "react";
 
 type OrderedItemType = {
-	firstName: string;
-	lastName: string;
+	imgageURL: string;
+	bookTitle: string;
+	quantity: string;
 	total: number;
 };
 export default function OrderedItem({
-	firstName,
-	lastName,
+	imgageURL,
+	bookTitle,
+	quantity,
 	total,
 }: OrderedItemType) {
-	return <React.Fragment>hi</React.Fragment>;
+	return (
+		<React.Fragment>
+			{" "}
+			<div className="flex items-center justify-center py-2">
+				<div className="w-20">
+					<img
+						src={imgageURL}
+						alt=""
+					/>
+				</div>
+				<div className="px-8">
+					<h2 className="block text-gray-400 font-semibold text-2xl">
+						{bookTitle}
+					</h2>
+					<div className="">
+						<p className="inline-block px-2 text-gray-500 text-lg">
+							Quantity: {quantity}
+						</p>
+						<p className="inline-block px-2 text-gray-500 text-lg">
+							Amount: $ {total}
+						</p>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 }
