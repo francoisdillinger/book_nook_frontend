83d757ffca43eb3fd7e7803325bcc61eac1c80f7
francoisdillinger <elmojester@gmail.com>
2024-03-08 23:30:54 -0600

    Refactor order reformatting function and remove console.log

--- src/components/AdminOrders.tsx
+++ src/components/AdminOrders.tsx
@@ -3,45 +3,6 @@ import TableData from "./TableData";
 import { orders_data, OrdersType } from "../data/orders_data";
 import SearchBar from "./SearchBar";
 
-const reformateOrders = (
-	orders: TrimmedOrdersType[]
-): ReformatedOrdersType[] => {
-	const orderIds = orders.map((order) => order.orderId);
-	const removedDuplicateIds = [...new Set(orderIds)];
-
-	const newOrders = removedDuplicateIds.map((id) => {
-		const matchingOrders = orders.filter((order) => id === order.orderId);
-		const reformatedOrder = {
-			orderId: matchingOrders[0].orderId,
-			userId: matchingOrders[0].userId,
-			firstName: matchingOrders[0].user.firstName,
-			lastName: matchingOrders[0].user.lastName,
-			orderDate: matchingOrders[0].orderDate,
-			orderStatus: matchingOrders[0].orderStatus,
-			totalAmount: matchingOrders.reduce(
-				(accumulator, order) => accumulator + order.orderAmount,
-				0
-			),
-			totalQuantity: matchingOrders.reduce(
-				(accumulator, order) => accumulator + order.quantity,
-				0
-			),
-			orders: matchingOrders.map((order) => {
-				return {
-					bookId: order.bookId,
-					bookTitle: order.book.bookTitle,
-					isbn: order.book.isbn,
-					orderAmount: order.orderAmount,
-					quantity: order.quantity,
-				};
-			}),
-		};
-		return reformatedOrder;
-	});
-	// console.log("Orders: ", newOrders);
-	return newOrders;
-};
-
 const sortOrders = (
 	orders: ReformatedOrdersType[],
 	sortOption: OrdersSortType
