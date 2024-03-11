c7613bad192a513ee197c2a163e3b1b813dee6fa
francoisdillinger <elmojester@gmail.com>
2024-03-11 18:38:29 -0500

    Add author information to books

--- src/data/adminBooks.ts
+++ src/data/adminBooks.ts
@@ -8,6 +8,10 @@ export type BooksType = {
 			description: string;
 			inventoryCount: number;
 			isbn: string;
+			author: {
+				authorFirstName: string;
+				authorLastName: string;
+			};
 		}[];
 	};
 };
@@ -23,6 +27,10 @@ export const books: BooksType = {
 				description: "A dystopian novel by English novelist George Orwell.",
 				inventoryCount: 120,
 				isbn: "978-0451524935",
+				author: {
+					authorFirstName: "George",
+					authorLastName: "Orwell",
+				},
 			},
 			{
 				bookTitle: "Animal Farm",
@@ -33,6 +41,10 @@ export const books: BooksType = {
 					"A fairy story by George Orwell, portraying a revolution by farm animals against humans.",
 				inventoryCount: 100,
 				isbn: "978-0451526342",
+				author: {
+					authorFirstName: "George",
+					authorLastName: "Orwell",
+				},
 			},
 			{
 				bookTitle: "To the Lighthouse",
@@ -43,6 +55,10 @@ export const books: BooksType = {
 					"The Ramsays and their eight children vacation with an assortment of scholarly and artistic houseguests by the sea.",
 				inventoryCount: 85,
 				isbn: "978-0156907392",
+				author: {
+					authorFirstName: "Virginia",
+					authorLastName: "Woolf",
+				},
 			},
 			{
 				bookTitle: "Mrs Dalloway",
@@ -53,6 +69,10 @@ export const books: BooksType = {
 					"The novel details a day in the life of Clarissa Dalloway, a fictional high-society woman in post–First World War England.",
 				inventoryCount: 75,
 				isbn: "978-0156628709",
+				author: {
+					authorFirstName: "Virginia",
+					authorLastName: "Woolf",
+				},
 			},
 			{
 				bookTitle: "The Old Man and The Sea",
@@ -63,6 +83,10 @@ export const books: BooksType = {
 					"The story of an old Cuban fisherman and his supreme ordeal: a relentless, agonizing battle with a giant marlin far out in the Gulf Stream.",
 				inventoryCount: 100,
 				isbn: "978-0684801223",
+				author: {
+					authorFirstName: "Ernest",
+					authorLastName: "Hemingway",
+				},
 			},
 			{
 				bookTitle: "A Farewell to Arms",
@@ -73,6 +97,10 @@ export const books: BooksType = {
 					"A novel written by Ernest Hemingway set during the Italian campaign of World War I.",
 				inventoryCount: 80,
 				isbn: "978-0684801469",
+				author: {
+					authorFirstName: "Ernest",
+					authorLastName: "Hemingway",
+				},
 			},
 			{
 				bookTitle: "Ulysses",
@@ -83,6 +111,10 @@ export const books: BooksType = {
 					"The novel imitates registers of centuries of English literature and is highly allusive.",
 				inventoryCount: 60,
 				isbn: "978-0679722762",
+				author: {
+					authorFirstName: "James",
+					authorLastName: "Joyce",
+				},
 			},
 			{
 				bookTitle: "Finnegans Wake",
@@ -93,6 +125,10 @@ export const books: BooksType = {
 					"A work of fiction by Irish writer James Joyce, significant for its experimental style.",
 				inventoryCount: 40,
 				isbn: "978-0141181264",
+				author: {
+					authorFirstName: "James",
+					authorLastName: "Joyce",
+				},
 			},
 			{
 				bookTitle: "Wuthering Heights",
@@ -103,6 +139,10 @@ export const books: BooksType = {
 					"A novel by Emily Bronte, it follows the life of Heathcliff, a mysterious gypsy-like person.",
 				inventoryCount: 50,
 				isbn: "978-0553212587",
+				author: {
+					authorFirstName: "Emily",
+					authorLastName: "Bronte",
+				},
 			},
 			{
 				bookTitle: "Pride and Prejudice",
@@ -112,6 +152,10 @@ export const books: BooksType = {
 				description: "A romantic novel of manners written by Jane Austen.",
 				inventoryCount: 120,
 				isbn: "978-1503290563",
+				author: {
+					authorFirstName: "Jane",
+					authorLastName: "Austen",
+				},
 			},
 			{
 				bookTitle: "Emma",
@@ -122,6 +166,10 @@ export const books: BooksType = {
 					"A novel about youthful hubris and romantic misunderstandings. It is set in the fictional country village of Highbury and the surrounding estates of Hartfield, Randalls, and Donwell Abbey.",
 				inventoryCount: 70,
 				isbn: "978-0141439587",
+				author: {
+					authorFirstName: "Jane",
+					authorLastName: "Austen",
+				},
 			},
 			{
 				bookTitle: "Great Expectations",
@@ -132,6 +180,10 @@ export const books: BooksType = {
 					"The novel, written by Charles Dickens, is a bildungsroman, a coming-of-age story, and it is a classic work of Victorian literature.",
 				inventoryCount: 85,
 				isbn: "978-0141439563",
+				author: {
+					authorFirstName: "Charles",
+					authorLastName: "Dickens",
+				},
 			},
 			{
 				bookTitle: "Oliver Twist",
@@ -142,6 +194,10 @@ export const books: BooksType = {
 					"The novel tells the story of an orphan boy and his experiences with crime and hardship in London.",
 				inventoryCount: 65,
 				isbn: "978-0486424538",
+				author: {
+					authorFirstName: "Charles",
+					authorLastName: "Dickens",
+				},
 			},
 			{
 				bookTitle: "Adventures of Huckleberry Finn",
@@ -152,6 +208,10 @@ export const books: BooksType = {
 					"A novel by Mark Twain, it is commonly named among the Great American Novels. The work is among the first in major American literature to be written throughout in vernacular English.",
 				inventoryCount: 90,
 				isbn: "978-0486449900",
+				author: {
+					authorFirstName: "Mark",
+					authorLastName: "Twain",
+				},
 			},
 			{
 				bookTitle: "The Adventures of Tom Sawyer",
@@ -162,6 +222,10 @@ export const books: BooksType = {
 					"The novel is set in the 1840s in the town of St. Petersburg, which is based on Hannibal, Missouri where Twain lived as a boy.",
 				inventoryCount: 80,
 				isbn: "978-0486400778",
+				author: {
+					authorFirstName: "Mark",
+					authorLastName: "Twain",
+				},
 			},
 			{
 				bookTitle: "To Kill a Mockingbird",
@@ -172,6 +236,10 @@ export const books: BooksType = {
 					"The novel is renowned for its warmth and humor, despite dealing with serious issues of rape and racial inequality.",
 				inventoryCount: 120,
 				isbn: "978-0446310789",
+				author: {
+					authorFirstName: "Harper",
+					authorLastName: "Lee",
+				},
 			},
 			{
 				bookTitle: "Go Set a Watchman",
@@ -182,6 +250,10 @@ export const books: BooksType = {
 					"Written in the mid-1950s, the novel was published by Harper Lee in 2015, as a follow-up to To Kill a Mockingbird.",
 				inventoryCount: 70,
 				isbn: "978-0062409850",
+				author: {
+					authorFirstName: "Harper",
+					authorLastName: "Lee",
+				},
 			},
 			{
 				bookTitle: "The Catcher in the Rye",
@@ -192,6 +264,10 @@ export const books: BooksType = {
 					"The novel's protagonist, Holden Caulfield, has become an icon for teenage rebellion and angst.",
 				inventoryCount: 95,
 				isbn: "978-7543321724",
+				author: {
+					authorFirstName: "J.D.",
+					authorLastName: "Salinger",
+				},
 			},
 			{
 				bookTitle: "Nine Stories",
@@ -202,6 +278,10 @@ export const books: BooksType = {
 					"A collection of short stories by J.D. Salinger. It includes two of his most famous short stories, 'A Perfect Day for Bananafish' and 'For Esmé – with Love and Squalor'.",
 				inventoryCount: 75,
 				isbn: "978-0316769495",
+				author: {
+					authorFirstName: "J.D.",
+					authorLastName: "Salinger",
+				},
 			},
 			{
 				bookTitle: "Franny and Zooey",
@@ -212,6 +292,10 @@ export const books: BooksType = {
 					"The book comprises his short story 'Franny' and novella 'Zooey', both of which revolve around the two youngest members of the Glass family.",
 				inventoryCount: 65,
 				isbn: "978-0316769495",
+				author: {
+					authorFirstName: "J.D.",
+					authorLastName: "Salinger",
+				},
 			},
 		],
 	},
