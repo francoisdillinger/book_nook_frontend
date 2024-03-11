b307fa7e1d1b2925a294e3712b1e65c2d0fde6f5
francoisdillinger <elmojester@gmail.com>
2024-03-11 17:11:59 -0500

    Refactor AdminBooks component to trim orders

--- src/components/AdminBooks.tsx
+++ src/components/AdminBooks.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { books, BooksType } from "../data/adminBooks";
+
+type TrimmedBookType = {
+	bookTitle: string;
+	pageCount: number;
+	publishDate: string;
+	price: number;
+	description: string;
+	inventoryCount: number;
+	isbn: string;
+};
+
+const trimOrders = (books: BooksType): TrimmedBookType[] => {
+	return [...books.data.books];
+};
 
 export default function AdminBooks() {
+	console.log(trimOrders(books));
 	return <>hi</>;
 }
