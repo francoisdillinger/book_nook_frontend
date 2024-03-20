839d398744367b7f47867002efab89858d4b3593
francoisdillinger <elmojester@gmail.com>
2024-03-20 18:34:45 -0500

    Update book price to publish date

--- src/components/AdminBooks.tsx
+++ src/components/AdminBooks.tsx
@@ -189,7 +189,7 @@ export default function AdminBooks() {
 							" " +
 							book.author.authorLastName +
 							" : " +
-							book.price}
+							book.publishDate}
 					</li>
 				))}
 			</ul>
