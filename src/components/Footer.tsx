a195cd8d7a39493f686836a2e582a722fcfb38f4
francoisdillinger <elmojester@gmail.com>
2024-03-23 02:17:18 -0500

    Update Footer component styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -51,7 +51,7 @@ export default function Footer() {
 									<path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" />
 								</svg>
 							</div>
-							<div className="w-11 h-11 border rounded-lg flex justify-center items-center shadow-sm transition ease-in-out delay-100 hover:bg-gray-200">
+							<div className="cursor-pointer w-11 h-11 border rounded-lg flex justify-center items-center shadow-sm transition ease-in-out delay-100 hover:bg-gray-200">
 								{/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
 								<svg
 									className="h-6 fill-black"
@@ -62,7 +62,7 @@ export default function Footer() {
 									<path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
 								</svg>
 							</div>
-							<div className="w-11 h-11 border rounded-lg flex justify-center items-center shadow-sm transition ease-in-out delay-100 hover:bg-sky-100">
+							<div className="cursor-pointer w-11 h-11 border rounded-lg flex justify-center items-center shadow-sm transition ease-in-out delay-100 hover:bg-sky-100">
 								{/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
 								<svg
 									className="h-6 fill-sky-600"
@@ -73,7 +73,7 @@ export default function Footer() {
 									<path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
 								</svg>
 							</div>
-							<div className="w-11 h-11 border rounded-lg flex justify-center items-center shadow-sm transition ease-in-out delay-100 hover:bg-rose-100">
+							<div className="cursor-pointer w-11 h-11 border rounded-lg flex justify-center items-center shadow-sm transition ease-in-out delay-100 hover:bg-rose-100">
 								{/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
 								<svg
 									className="h-6 fill-rose-400"
