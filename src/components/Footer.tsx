1898a4a718af786d5c4500d578816b4053c66bc9
francoisdillinger <elmojester@gmail.com>
2024-03-23 02:16:50 -0500

    Update Footer component with cursor pointer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -40,7 +40,7 @@ export default function Footer() {
 									<path d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z" />
 								</svg>
 							</div>
-							<div className="w-11 h-11 border rounded-lg flex justify-center items-center shadow-sm transition ease-in-out delay-100 hover:bg-rose-200">
+							<div className="cursor-pointer w-11 h-11 border rounded-lg flex justify-center items-center shadow-sm transition ease-in-out delay-100 hover:bg-rose-200">
 								{/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
 								<svg
 									className="h-6 fill-rose-600"
