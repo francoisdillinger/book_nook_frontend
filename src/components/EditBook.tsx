1d43f8368137b5e714deb605ed024afcc26c4966
francoisdillinger <elmojester@gmail.com>
2024-03-26 23:20:56 -0500

    Update form layout in EditBook component

--- src/components/EditBook.tsx
+++ src/components/EditBook.tsx
@@ -2,7 +2,7 @@ export default function EditBook() {
 	return (
 		<form
 			action=""
-			className="w-3/4 m-auto font-medium text-gray-500 text-sm"
+			className="w-11/12 md:w-3/4 m-auto font-medium text-gray-500 text-sm"
 		>
 			<div className="">
 				<label
@@ -12,7 +12,7 @@ export default function EditBook() {
 					Book Image
 				</label>
 				<input
-					className="block file:p-3 file:bg-logo file:border-none file:text-gray-50 file:opacity-90 file:hover:opacity-100 file:cursor-pointer file:font-medium w-1/2 text-sm font-normal text-gray-500 border border-gray-300 rounded-md bg-gray-50 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
+					className="block file:p-3 file:bg-logo file:border-none file:text-gray-50 file:opacity-90 file:hover:opacity-100 file:cursor-pointer file:font-medium w-full md:w-1/2 text-sm font-normal text-gray-500 border border-gray-300 rounded-md bg-gray-50 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
 					type="file"
 					name="image"
 					id="image"
@@ -26,7 +26,7 @@ export default function EditBook() {
 					Title
 				</label>
 				<input
-					className="block p-2.5 w-1/2 z-20 text-sm font-normal text-gray-700 rounded-md bg-gray-50 border border-gray-300 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
+					className="block p-2.5 w-full md:w-1/2 z-20 text-sm font-normal text-gray-700 rounded-md bg-gray-50 border border-gray-300 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
 					type="text"
 					name="title"
 					id="title"
@@ -40,7 +40,7 @@ export default function EditBook() {
 					Author First Name
 				</label>
 				<input
-					className="block p-2.5 w-1/2 z-20 text-sm font-normal text-gray-700 rounded-md bg-gray-50 border border-gray-300 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
+					className="block p-2.5 w-full md:w-1/2 z-20 text-sm font-normal text-gray-700 rounded-md bg-gray-50 border border-gray-300 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
 					type="text"
 					name="firstname"
 					id="firstname"
@@ -54,7 +54,7 @@ export default function EditBook() {
 					Author Last Name
 				</label>
 				<input
-					className="block p-2.5 w-1/2 z-20 text-sm font-normal text-gray-700 rounded-md bg-gray-50 border border-gray-300 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
+					className="block p-2.5 w-full md:w-1/2 z-20 text-sm font-normal text-gray-700 rounded-md bg-gray-50 border border-gray-300 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
 					type="text"
 					name="lastname"
 					id="lastname"
