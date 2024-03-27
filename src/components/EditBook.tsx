784ebec0d34b708b4868876b5558d3b5fa4f9972
francoisdillinger <elmojester@gmail.com>
2024-03-26 22:58:12 -0500

    Add form for editing book details

--- src/components/EditBook.tsx
+++ src/components/EditBook.tsx
@@ -1,3 +1,46 @@
 export default function EditBook() {
-	return <>Hi</>;
+	return (
+		<form
+			action=""
+			className=""
+		>
+			<div className="">
+				<label
+					htmlFor="image"
+					className="block"
+				>
+					Book Image
+				</label>
+				<input
+					type="file"
+					name="image"
+					id="image"
+				/>
+			</div>
+			<div className="">
+				<label htmlFor="title">Title</label>
+				<input
+					type="text"
+					name="title"
+					id="title"
+				/>
+			</div>
+			<div className="">
+				<label htmlFor="firstname">Author First Name</label>
+				<input
+					type="text"
+					name="firstname"
+					id="firstname"
+				/>
+			</div>
+			<div className="">
+				<label htmlFor="lastname">Author Last Name</label>
+				<input
+					type="text"
+					name="lastname"
+					id="lastname"
+				/>
+			</div>
+		</form>
+	);
 }
