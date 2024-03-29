4ba067597da4b5260a90984a06d519a38d46f549
francoisdillinger <elmojester@gmail.com>
2024-03-28 21:49:39 -0500

    Update submit button label

--- src/components/EditBook.tsx
+++ src/components/EditBook.tsx
@@ -151,7 +151,7 @@ export default function EditBook() {
 						type="submit"
 						className="bg-logo p-3 rounded-md w-full md:w-1/2 text-gray-50 opacity-90 hover:opacity-100 cursor-pointer font-medium"
 					>
-						Submit
+						Update
 					</button>
 				</div>
 				{/* <div className="">
