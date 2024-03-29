46d01f57819822f4844b1f7fc89376d23ca38d9e
francoisdillinger <elmojester@gmail.com>
2024-03-28 22:10:27 -0500

    Add label for Author in AddBook component

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -116,6 +116,12 @@ export default function AddBook() {
 					></textarea>
 				</div>
 				<div className="">
+					<label
+						htmlFor="publishedDate"
+						className="block"
+					>
+						Author
+					</label>
 					<select
 						id="book-selector"
 						className="block p-2.5 w-full lg:w-full z-20 text-sm font-normal text-gray-700 rounded-md bg-gray-50 border border-gray-300 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
