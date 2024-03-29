800331885b8cb180a6ec4017482ce0bd019ec38c
francoisdillinger <elmojester@gmail.com>
2024-03-28 22:10:04 -0500

    Add book selector component

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -116,6 +116,17 @@ export default function AddBook() {
 					></textarea>
 				</div>
 				<div className="">
+					<select
+						id="book-selector"
+						className="block p-2.5 w-full lg:w-full z-20 text-sm font-normal text-gray-700 rounded-md bg-gray-50 border border-gray-300 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
+						// onChange={(event) => selectOptionsHandler(event)}
+					>
+						{/* {options.map((option) => (
+							<option key={option}>{option}</option>
+						))} */}
+					</select>
+				</div>
+				<div className="">
 					<label
 						htmlFor="inventory"
 						className="block"
