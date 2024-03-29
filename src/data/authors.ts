2513c604f11ebd188b23e03ffe3daceca73fe692
francoisdillinger <elmojester@gmail.com>
2024-03-28 22:12:06 -0500

    Add authors data to the project

---
+++ src/data/authors.ts
@@ -0,0 +1,46 @@
+export const authors = {
+	data: {
+		authors: [
+			{
+				authorFirstName: "George",
+				authorLastName: "Orwell",
+			},
+			{
+				authorFirstName: "Virginia",
+				authorLastName: "Woolf",
+			},
+			{
+				authorFirstName: "Ernest",
+				authorLastName: "Hemingway",
+			},
+			{
+				authorFirstName: "James",
+				authorLastName: "Joyce",
+			},
+			{
+				authorFirstName: "Emily",
+				authorLastName: "Bronte",
+			},
+			{
+				authorFirstName: "Jane",
+				authorLastName: "Austen",
+			},
+			{
+				authorFirstName: "Charles",
+				authorLastName: "Dickens",
+			},
+			{
+				authorFirstName: "Mark",
+				authorLastName: "Twain",
+			},
+			{
+				authorFirstName: "Harper",
+				authorLastName: "Lee",
+			},
+			{
+				authorFirstName: "J.D.",
+				authorLastName: "Salinger",
+			},
+		],
+	},
+};
