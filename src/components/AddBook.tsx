8811a8fb506d2dd5fa295099c668299486c0a9c2
francoisdillinger <elmojester@gmail.com>
2024-03-28 22:01:42 -0500

    Update button text in AddBook component

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -152,7 +152,7 @@ export default function AddBook() {
 						type="submit"
 						className="bg-logo p-3 rounded-md w-full md:w-1/2 text-gray-50 opacity-90 hover:opacity-100 cursor-pointer font-medium"
 					>
-						Update
+						Add Book
 					</button>
 				</div>
 			</form>
