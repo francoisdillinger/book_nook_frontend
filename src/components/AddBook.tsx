27f3c78fd5b35df55df3e548201d4278ffd47929
francoisdillinger <elmojester@gmail.com>
2024-03-28 21:52:25 -0500

    Add handleFileChange function to handle file input

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -2,5 +2,12 @@ import React, { useState } from "react";
 
 export default function AddBook() {
 	const [image, setImage] = useState<string>("");
+
+	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		console.log(event.target.files);
+		if (event.target.files && event.target.files.length > 0) {
+			setImage(URL.createObjectURL(event.target.files[0]));
+		}
+	};
 	return <></>;
 }
