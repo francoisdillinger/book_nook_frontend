648c4a1e02c730fcf8bf753836f5fdfc7a6a29aa
francoisdillinger <elmojester@gmail.com>
2024-03-28 21:54:08 -0500

    Add book image input field

--- src/components/AddBook.tsx
+++ src/components/AddBook.tsx
@@ -30,7 +30,23 @@ export default function AddBook() {
 			<form
 				action=""
 				className="w-11/12 md:w-1/2 m-auto mb-20 font-medium text-gray-500 text-sm"
-			></form>
+			>
+				<div className="">
+					<label
+						htmlFor="image"
+						className="block"
+					>
+						Book Image
+					</label>
+					<input
+						onChange={handleFileChange}
+						className="block file:p-3 file:bg-logo file:border-none file:text-gray-50 file:opacity-90 file:hover:opacity-100 file:cursor-pointer file:font-medium w-full lg:w-full text-sm font-normal text-gray-500 border border-gray-300 rounded-md bg-gray-50 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
+						type="file"
+						name="image"
+						id="image"
+					/>
+				</div>
+			</form>
 		</React.Fragment>
 	);
 }
