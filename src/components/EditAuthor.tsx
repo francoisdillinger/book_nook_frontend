a20ba2f415dc7783a4c6be0080029df000f9caf7
francoisdillinger <elmojester@gmail.com>
2024-03-30 23:34:15 -0500

    Add author selection dropdown in EditAuthor component

--- src/components/EditAuthor.tsx
+++ src/components/EditAuthor.tsx
@@ -18,6 +18,26 @@ export default function EditAuthor() {
 			>
 				<div className="">
 					<label
+						htmlFor="author"
+						className="block"
+					>
+						Author
+					</label>
+					<select
+						id="author"
+						className="block p-2.5 w-full lg:w-full z-20 text-sm font-normal text-gray-700 rounded-md bg-gray-50 border border-gray-300 focus:ring-blue-300 focus:border-blue-300 focus-visible:outline-none"
+						// onChange={(event) => selectOptionsHandler(event)}
+					>
+						<option value="">--- Select Author ---</option>
+						{authors?.map((author, index) => (
+							<option key={index}>
+								{author.authorFirstName + " " + author.authorLastName}
+							</option>
+						))}
+					</select>
+				</div>
+				<div className="">
+					<label
 						htmlFor="firstname"
 						className="block"
 					>
