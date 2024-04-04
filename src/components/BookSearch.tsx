8c18c1fb53da31692628cd3b2a085e3bc5ec4197
francoisdillinger <elmojester@gmail.com>
2024-04-04 00:47:27 -0500

    Add useState and useEffect hooks, define BookType interface

--- src/components/BookSearch.tsx
+++ src/components/BookSearch.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { books, BooksType } from "../data/books";
+
+export type BookType = {
+	bookTitle: string;
+	pageCount: number;
+	publishDate: string;
+	price: number;
+	description: string;
+	inventoryCount: number;
+	isbn: string;
+};
 
 export default function BookSearch() {
+	const [books, setBooks] = useState<BookType[] | null>();
+
 	return <React.Fragment>hi</React.Fragment>;
 }
