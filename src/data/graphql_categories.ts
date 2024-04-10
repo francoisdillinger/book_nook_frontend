9132f50ffb8de470a0dd2b403596e9947cc0faa1
francoisdillinger <elmojester@gmail.com>
2024-04-09 21:42:08 -0500

    Add graphql_categories data

---
+++ src/data/graphql_categories.ts
@@ -0,0 +1,363 @@
+export const graphql_categories = {
+	data: {
+		categories: [
+			{
+				categoryName: "Inspirational",
+			},
+			{
+				categoryName: "Coming-of-Age",
+			},
+			{
+				categoryName: "Children’s Literature",
+			},
+			{
+				categoryName: "Drama",
+			},
+			{
+				categoryName: "Horror",
+			},
+			{
+				categoryName: "Gender Studies",
+			},
+			{
+				categoryName: "Feminist Theory",
+			},
+			{
+				categoryName: "Postmodern",
+			},
+			{
+				categoryName: "Southern Fiction",
+			},
+			{
+				categoryName: "Magical Realism",
+			},
+			{
+				categoryName: "Memoir",
+			},
+			{
+				categoryName: "Psychological Thriller",
+			},
+			{
+				categoryName: "Children’s Poetry",
+			},
+			{
+				categoryName: "Philosophical Fiction",
+			},
+			{
+				categoryName: "Philosophical",
+			},
+			{
+				categoryName: "Epic",
+			},
+			{
+				categoryName: "Feminist Literature",
+			},
+			{
+				categoryName: "Semi-autobiographical",
+			},
+			{
+				categoryName: "Dystopian Fiction",
+			},
+			{
+				categoryName: "Historical Fantasy",
+			},
+			{
+				categoryName: "Satire",
+			},
+			{
+				categoryName: "Romance",
+			},
+			{
+				categoryName: "Cultural",
+			},
+			{
+				categoryName: "Existential Fiction",
+			},
+			{
+				categoryName: "Realism",
+			},
+			{
+				categoryName: "Poetry",
+			},
+			{
+				categoryName: "Beat Generation",
+			},
+			{
+				categoryName: "Detective Fiction",
+			},
+			{
+				categoryName: "Post-Apocalyptic",
+			},
+			{
+				categoryName: "Modernist",
+			},
+			{
+				categoryName: "Children’s Fantasy",
+			},
+			{
+				categoryName: "Social Commentary",
+			},
+			{
+				categoryName: "Alternate History",
+			},
+			{
+				categoryName: "Historical",
+			},
+			{
+				categoryName: "Modernist Literature",
+			},
+			{
+				categoryName: "Adventure",
+			},
+			{
+				categoryName: "Classic Romance",
+			},
+			{
+				categoryName: "Family Saga",
+			},
+			{
+				categoryName: "Non-fiction",
+			},
+			{
+				categoryName: "Southern Gothic",
+			},
+			{
+				categoryName: "Political Satire",
+			},
+			{
+				categoryName: "Mystery",
+			},
+			{
+				categoryName: "Legal Thriller",
+			},
+			{
+				categoryName: "Tragedy",
+			},
+			{
+				categoryName: "Science Fiction",
+			},
+			{
+				categoryName: "Road Novel",
+			},
+			{
+				categoryName: "Postmodernism",
+			},
+			{
+				categoryName: "Mythology",
+			},
+			{
+				categoryName: "Allegorical",
+			},
+			{
+				categoryName: "Fantasy",
+			},
+			{
+				categoryName: "Psychological Fiction",
+			},
+			{
+				categoryName: "War",
+			},
+			{
+				categoryName: "Classical",
+			},
+			{
+				categoryName: "Self-Help",
+			},
+			{
+				categoryName: "Literary Realism",
+			},
+			{
+				categoryName: "Gothic",
+			},
+			{
+				categoryName: "Historical Romance",
+			},
+			{
+				categoryName: "Classical Literature",
+			},
+			{
+				categoryName: "Self-Help",
+			},
+			{
+				categoryName: "Literary Realism",
+			},
+			{
+				categoryName: "Detective Fiction",
+			},
+			{
+				categoryName: "War Fiction",
+			},
+			{
+				categoryName: "Science Fiction",
+			},
+			{
+				categoryName: "Fantasy Adventure",
+			},
+			{
+				categoryName: "Political Fiction",
+			},
+			{
+				categoryName: "Historical Fiction",
+			},
+			{
+				categoryName: "Modern Classics",
+			},
+			{
+				categoryName: "Psychoanalytic Fiction",
+			},
+			{
+				categoryName: "Surrealist Fiction",
+			},
+			{
+				categoryName: "Biography",
+			},
+			{
+				categoryName: "Autobiography",
+			},
+			{
+				categoryName: "Epic Poetry",
+			},
+			{
+				categoryName: "Short Stories",
+			},
+			{
+				categoryName: "Speculative Fiction",
+			},
+			{
+				categoryName: "Urban Fantasy",
+			},
+			{
+				categoryName: "Mythology",
+			},
+			{
+				categoryName: "Fairy Tales",
+			},
+			{
+				categoryName: "Adventure Fiction",
+			},
+			{
+				categoryName: "Crime Fiction",
+			},
+			{
+				categoryName: "High Fantasy",
+			},
+			{
+				categoryName: "Young Adult Fiction",
+			},
+			{
+				categoryName: "Post-Colonial Literature",
+			},
+			{
+				categoryName: "Existential Philosophy",
+			},
+			{
+				categoryName: "Absurdist Fiction",
+			},
+			{
+				categoryName: "Beat Literature",
+			},
+			{
+				categoryName: "Graphic Novels",
+			},
+			{
+				categoryName: "Ecofiction",
+			},
+			{
+				categoryName: "Cyberpunk",
+			},
+			{
+				categoryName: "Hard Science Fiction",
+			},
+			{
+				categoryName: "Space Opera",
+			},
+			{
+				categoryName: "Steam Punk",
+			},
+			{
+				categoryName: "Dystopian",
+			},
+			{
+				categoryName: "Utopian",
+			},
+			{
+				categoryName: "Alternative History",
+			},
+			{
+				categoryName: "Mystery Thriller",
+			},
+			{
+				categoryName: "Legal Drama",
+			},
+			{
+				categoryName: "Military Fiction",
+			},
+			{
+				categoryName: "Romantic Comedy",
+			},
+			{
+				categoryName: "Dark Fantasy",
+			},
+			{
+				categoryName: "Supernatural Fiction",
+			},
+			{
+				categoryName: "Paranormal Romance",
+			},
+			{
+				categoryName: "Literary Fiction",
+			},
+			{
+				categoryName: "Narrative Nonfiction",
+			},
+			{
+				categoryName: "Creative Nonfiction",
+			},
+			{
+				categoryName: "Humor",
+			},
+			{
+				categoryName: "Satirical Fiction",
+			},
+			{
+				categoryName: "Classical Mythology",
+			},
+			{
+				categoryName: "Philosophy",
+			},
+			{
+				categoryName: "Educational",
+			},
+			{
+				categoryName: "Self-Improvement",
+			},
+			{
+				categoryName: "Travel Literature",
+			},
+			{
+				categoryName: "Nature Writing",
+			},
+			{
+				categoryName: "Science",
+			},
+			{
+				categoryName: "Historical Mystery",
+			},
+			{
+				categoryName: "Religious Fiction",
+			},
+			{
+				categoryName: "Spiritual",
+			},
+			{
+				categoryName: "Gothic Horror",
+			},
+			{
+				categoryName: "Pulp Fiction",
+			},
+			{
+				categoryName: "Action and Adventure",
+			},
+		],
+	},
+};
