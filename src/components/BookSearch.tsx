ee597eeb7554840f4c6e42945f7c872b5dc0c3b2
francoisdillinger <elmojester@gmail.com>
2024-04-18 15:36:29 -0500

    Update BookSearch component's heading

--- src/components/BookSearch.tsx
+++ src/components/BookSearch.tsx
@@ -410,7 +410,7 @@ export default function BookSearch() {
 				<div className="pt-6 pb-12 w-10/12">
 					<div className="w-full px-4 pb-2 md:w-11/12 m-auto md:p-0 md:py-4 flex-wrap md:flex md:justify-between">
 						<div>
-							<h1 className="font-semibold text-3xl text-gray-400">Results:</h1>
+							<h1 className="font-semibold text-3xl text-gray-400">Results</h1>
 						</div>
 						<div className="">
 							<select
