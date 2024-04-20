78bc2e35040d5c5ce527a4bf7054dad57595cf05
francoisdillinger <elmojester@gmail.com>
2024-04-20 00:35:10 -0500

    Refactor BookItem component layout

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -35,23 +35,28 @@ export default function BookItem() {
 						alt={`Image for ${bookItem?.bookTitle}`}
 					/>
 				</div>
-				<div>
-					<h1 className="text-5xl text-gray-500">{bookItem?.bookTitle}</h1>
-					<h2 className="text-2xl text-gray-400 pb-2">
-						{book.author.authorFirstName + " " + book.author.authorLastName}
-					</h2>
-					<div className="flex items-center">
-						<p className="text-2xl text-gray-500">{bookRating}</p>
-						<span className="pl-2">
-							<StaticStarRating
-								rating={bookRating}
-								width={7}
-								height={7}
-							/>
-						</span>
-						<p className="text-lg text-gray-400 font-medium pl-6">
-							{bookItem?.bookReviews?.length} ratings
-						</p>
+				<div className="w-1/2 flex flex-col justify-between">
+					<div>
+						<h1 className="text-5xl text-gray-500">{bookItem?.bookTitle}</h1>
+						<h2 className="text-2xl text-gray-400 pb-2">
+							{book.author.authorFirstName + " " + book.author.authorLastName}
+						</h2>
+						<div className="flex items-center">
+							<p className="text-2xl text-gray-500">{bookRating}</p>
+							<span className="pl-2">
+								<StaticStarRating
+									rating={bookRating}
+									width={7}
+									height={7}
+								/>
+							</span>
+							<p className="text-lg text-gray-400 font-medium pl-6">
+								{bookItem?.bookReviews?.length} ratings
+							</p>
+						</div>
+					</div>
+					<div>
+						<p className="text-lg text-gray-500">{bookItem?.description}</p>
 					</div>
 				</div>
 			</div>
