f00b3fc3afca6560092c8618e2ba3e538d0eabdb
francoisdillinger <elmojester@gmail.com>
2024-04-19 23:56:49 -0500

    Add book cover image to BookItem component

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { TrimmedBookType } from "./AdminBooks";
 import { book } from "../data/bookItem";
+import alchemist from "../assets/alchemist.jpg";
 
 export default function BookItem() {
 	const [bookItem, setBookItem] = useState<null | TrimmedBookType>();
@@ -11,7 +12,14 @@ export default function BookItem() {
 	// console.log(bookItem);
 	return (
 		<React.Fragment>
-			<div></div>
+			<div className="">
+				<div>
+					<img
+						src={alchemist}
+						alt={`Image for ${bookItem?.bookTitle}`}
+					/>
+				</div>
+			</div>
 		</React.Fragment>
 	);
 }
