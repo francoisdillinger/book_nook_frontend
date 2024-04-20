0582090eb07b06a18db76d15b0c039a3def2681c
francoisdillinger <elmojester@gmail.com>
2024-04-19 23:48:52 -0500

    Add state and effect hooks to BookItem component

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+	booksWithCategories,
+	BookWithCategoriesType,
+} from "../data/booksWithCategories";
+import { TrimmedBookType } from "./AdminBooks";
 
 export default function BookItem() {
+	const [bookItem, setBookItem] = useState<null | TrimmedBookType>();
+
+	useEffect(() => {
+		const book = booksWithCategories.data.books[0];
+		setBookItem(book);
+	}, []);
+	console.log(bookItem?.description);
 	return <React.Fragment></React.Fragment>;
 }
