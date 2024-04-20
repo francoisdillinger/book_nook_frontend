398a8f0474b8ae25adbf5de8c326b80de10383c7
francoisdillinger <elmojester@gmail.com>
2024-04-20 00:03:04 -0500

    Add StaticStarRating component to BookItem

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { TrimmedBookType } from "./AdminBooks";
 import { book } from "../data/bookItem";
 import alchemist from "../assets/alchemist.jpg";
+import StaticStarRating from "./StaticStarRating";
 
 export default function BookItem() {
 	const [bookItem, setBookItem] = useState<null | TrimmedBookType>();
@@ -25,6 +26,19 @@ export default function BookItem() {
 					<h2 className="text-2xl text-gray-400 pb-2">
 						{book.author.authorFirstName + " " + book.author.authorLastName}
 					</h2>
+					<span>
+						{" "}
+						<StaticStarRating
+							rating={Math.floor(
+								book.bookReviews!.reduce(
+									(accumulator, review) => accumulator + review.rating,
+									0
+								)
+							)}
+							width={7}
+							height={7}
+						/>
+					</span>
 				</div>
 			</div>
 		</React.Fragment>
