32268f30fbc0693d0772a0d97bec26d97e5cac66
francoisdillinger <elmojester@gmail.com>
2024-04-22 23:31:05 -0500

    Update BookItem component styling

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -69,7 +69,7 @@ export default function BookItem() {
 					</div>
 				</div>
 				<div className="w-full md:w-1/4 ">
-					<div className="bg-gray-100 rounded-lg w-full flex flex-col items-center pb-8">
+					<div className="bg-gray-100 rounded-lg w-full flex flex-col items-center h-1/2">
 						<h3 className="text-logo text-3xl pt-8 pb-16">
 							${book.price.toFixed(2)}
 						</h3>
@@ -77,6 +77,7 @@ export default function BookItem() {
 							Add To Cart
 						</div>
 					</div>
+					<div></div>
 				</div>
 			</div>
 		</React.Fragment>
