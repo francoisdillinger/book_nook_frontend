e4f319bd1626171ece132ba392e86ca5fd2066bf
francoisdillinger <elmojester@gmail.com>
2024-04-22 23:52:23 -0500

    Add wishlist icon to BookItem component

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -81,6 +81,17 @@ export default function BookItem() {
 						<h3 className="text-xl font-medium text-gray-500">
 							Add to Wishlist
 						</h3>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 24 24"
+							className="w-6 h-6 fill-gray-100 stroke-2 stroke-gray-500"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+							/>
+						</svg>
 					</div>
 				</div>
 			</div>
