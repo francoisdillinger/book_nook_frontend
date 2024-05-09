130146377868899d72d4fca420147c98f1060ed6
francoisdillinger <elmojester@gmail.com>
2024-05-08 23:42:44 -0500

    Add framer-motion dependency to BookStandardCarousel component

--- src/components/BookStandardCarousel.tsx
+++ src/components/BookStandardCarousel.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { TrimmedBookType } from "./AdminBooks";
 import alchemist from "../assets/alchemist.jpg";
 import StaticStarRating from "./StaticStarRating";
+import { motion } from "framer-motion";
 
 export default function BookStandardCarousel() {
 	return <></>;
