9a85beb9f0236ae46174de68bca3cfe1eda47f8c
francoisdillinger <elmojester@gmail.com>
2024-05-09 14:48:22 -0500

    Refactor BookReview component to include user information and review details

--- src/components/BookReview.tsx
+++ src/components/BookReview.tsx
@@ -1,4 +1,38 @@
 import React from "react";
 export default function BookReview() {
-	return;
+	return (
+		<React.Fragment>
+			<div
+				key={index}
+				className="p-8"
+			>
+				<div className="flex items-center p-2">
+					<img
+						className="w-16 rounded-full shadow-lg"
+						src={memers}
+						alt=""
+					/>
+					<div className="pl-4">
+						<h1 className="text-gray-500 font-medium pl-1">
+							{review.user?.userName}
+						</h1>
+						<span className="">
+							<StaticStarRating
+								rating={review.rating}
+								width={6}
+								height={6}
+							/>
+						</span>
+						<p className="text-sm text-gray-400 pl-1">
+							Reviewed on {formattedDate}
+						</p>
+					</div>
+				</div>
+				<div className="text-center">
+					<h2 className="text-2xl text-gray-500 pb-4">{review.shortReview}</h2>
+					<p className="text-lg text-gray-600 font-light">{review.review}</p>
+				</div>
+			</div>
+		</React.Fragment>
+	);
 }
