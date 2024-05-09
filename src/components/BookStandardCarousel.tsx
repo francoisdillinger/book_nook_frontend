0ca0fe6b31ad65c821862c2e083a1b491e988bc7
francoisdillinger <elmojester@gmail.com>
2024-05-08 23:43:02 -0500

    Add booksWithCategories import to BookStandardCarousel component

--- src/components/BookStandardCarousel.tsx
+++ src/components/BookStandardCarousel.tsx
@@ -3,6 +3,10 @@ import { TrimmedBookType } from "./AdminBooks";
 import alchemist from "../assets/alchemist.jpg";
 import StaticStarRating from "./StaticStarRating";
 import { motion } from "framer-motion";
+import {
+	booksWithCategories,
+	BookWithCategoriesType,
+} from "../data/booksWithCategories";
 
 export default function BookStandardCarousel() {
 	return <></>;
