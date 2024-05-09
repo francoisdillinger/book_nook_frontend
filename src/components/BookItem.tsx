3e91b9168686354bc02932ddef9b0e9274131f0e
francoisdillinger <elmojester@gmail.com>
2024-05-09 14:55:32 -0500

    Refactor BookItem component to use BookReview component for displaying reviews

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -8,6 +8,7 @@ import BookStandardCarousel from "./BookStandardCarousel";
 import memers from "../assets/memers.jpg";
 import marms from "../assets/marms.jpg";
 import purpur from "../assets/purpur.jpg";
+import BookReview from "./BookReview";
 
 export const totalRating = (book: TrimmedBookType): number => {
 	const length = book.bookReviews ? book.bookReviews?.length : 1;
@@ -142,48 +143,14 @@ export default function BookItem() {
 							{bookItem?.bookReviews?.length} ratings
 						</p>
 					</div>
-					{book?.bookReviews?.map((review, index) => {
-						const now = new Date();
-						const options = { year: "numeric", month: "long", day: "numeric" };
-						const formattedDate = now.toLocaleDateString("en-US", options);
-						return (
-							<div
-								key={index}
-								className="p-8"
-							>
-								<div className="flex items-center p-2">
-									<img
-										className="w-16 rounded-full shadow-lg"
-										src={memers}
-										alt=""
-									/>
-									<div className="pl-4">
-										<h1 className="text-gray-500 font-medium pl-1">
-											{review.user?.userName}
-										</h1>
-										<span className="">
-											<StaticStarRating
-												rating={review.rating}
-												width={6}
-												height={6}
-											/>
-										</span>
-										<p className="text-sm text-gray-400 pl-1">
-											Reviewed on {formattedDate}
-										</p>
-									</div>
-								</div>
-								<div className="text-center">
-									<h2 className="text-2xl text-gray-500 pb-4">
-										{review.shortReview}
-									</h2>
-									<p className="text-lg text-gray-600 font-light">
-										{review.review}
-									</p>
-								</div>
-							</div>
-						);
-					})}
+					{book?.bookReviews?.map((review, index) => (
+						<BookReview
+							rating={review.rating}
+							review={review.review}
+							shortReview={review.shortReview}
+							user={review.user}
+						/>
+					))}
 				</div>
 			</section>
 		</React.Fragment>
