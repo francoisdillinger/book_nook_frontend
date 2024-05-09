57c2e702a77b42353ec12c31dae584a28cf14e0d
francoisdillinger <elmojester@gmail.com>
2024-05-08 23:42:31 -0500

    Add StaticStarRating component to BookStandardCarousel

--- src/components/BookStandardCarousel.tsx
+++ src/components/BookStandardCarousel.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { TrimmedBookType } from "./AdminBooks";
 import alchemist from "../assets/alchemist.jpg";
+import StaticStarRating from "./StaticStarRating";
 
 export default function BookStandardCarousel() {
 	return <></>;
