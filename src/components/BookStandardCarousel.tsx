6832710d8bebfd1cf5b7339806e28fc7a264ffd7
francoisdillinger <elmojester@gmail.com>
2024-05-08 23:42:18 -0500

    Add alchemist book cover image to BookStandardCarousel component

--- src/components/BookStandardCarousel.tsx
+++ src/components/BookStandardCarousel.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { TrimmedBookType } from "./AdminBooks";
+import alchemist from "../assets/alchemist.jpg";
 
 export default function BookStandardCarousel() {
 	return <></>;
