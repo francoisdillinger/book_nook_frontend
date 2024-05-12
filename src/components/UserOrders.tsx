013e600f7a684c478840d35fb1309f9bb292fdf6
francoisdillinger <elmojester@gmail.com>
2024-05-11 22:40:04 -0500

    Refactor UserOrders component to include useEffect and useState hooks

--- src/components/UserOrders.tsx
+++ src/components/UserOrders.tsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { orders, UserOrdersType } from "../data/userOrder";
+// import { trimOrders } from "../utils/adminOrdersUtilities";
+
+type UserOrder = {
+	orderId: string;
+	book: {
+		bookTitle: string;
+	};
+	quantity: number;
+	orderDate: string;
+	orderAmount: number;
+	orderStatus: string;
+};
+
+const trimOrders = (orders: UserOrdersType): UserOrder[] => {
+	return [...orders.orders];
+};
 
 export default function UserOrders() {
+	const [userOrders, setUserOrders] = useState<UserOrder[] | null>();
+	useEffect(() => {
+		const trimmedOrders = trimOrders(orders);
+		setUserOrders(trimmedOrders);
+	}, [orders]);
+	console.log(userOrders);
 	return <React.Fragment></React.Fragment>;
 }
