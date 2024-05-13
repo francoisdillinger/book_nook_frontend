791a6fdfe504d328d5503557c1d7b22ed192aa2e
francoisdillinger <elmojester@gmail.com>
2024-05-12 21:56:57 -0500

    Refactor UserOrders component to remove commented out code and update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ export default function App() {
 			{/* <SignUpForm /> */}
 			{/* <LoginForm /> */}
 			{/* <Profile /> */}
-			<UserOrders />
+			{/* <UserOrders /> */}
 			{/* <SideBarAdminMenu /> */}
 			{/* <AdminHome /> */}
 
@@ -52,7 +52,7 @@ export default function App() {
 				<AdminMenu /> 
 				<AdminChart />
 			</div> */}
-			{/* <AdminOrders /> */}
+			<AdminOrders />
 			{/* <AdminBooks /> */}
 			{/* <EditBook /> */}
 			{/* <AddBook /> */}
