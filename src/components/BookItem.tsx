312ebfbb55b5bde721415dfa68c83d5ed80c115a
francoisdillinger <elmojester@gmail.com>
2024-05-16 23:18:09 -0500

    Refactor BookItem.tsx to include hover animation in Add To Cart button

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -82,7 +82,7 @@ export default function BookItem() {
 						<h3 className="text-logo text-3xl pb-4">
 							${book.price.toFixed(2)}
 						</h3>
-						<div className="text-gray-600 bg-yellow-400 p-2 rounded-lg hover:bg-yellow-300 w-28 text-center cursor-pointer ease-in-out duration-300">
+						<div className="text-gray-600 bg-yellow-400 p-2 rounded-lg hover:bg-yellow-300 w-28 text-center cursor-pointer hover:scale-110 active:scale-100 ease-in-out duration-300">
 							Add To Cart
 						</div>
 					</div>
