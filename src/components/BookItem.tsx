c2cb742a3a375d2c286720def16ff35c2609d4c2
francoisdillinger <elmojester@gmail.com>
2024-05-25 23:01:57 -0500

    Refactor BookItem.tsx to optimize import paths and improve button hover animations

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -5,7 +5,7 @@ import alchemist from "../assets/alchemist.jpg";
 import StaticStarRating from "./StaticStarRating";
 import BookItemBadges from "./BookItemBadges";
 import BookStandardCarousel from "./BookStandardCarousel";
-// import memers from "../assets/memers.jpg";
+
 import BookReview from "./BookReview";
 
 export const totalRating = (book: TrimmedBookType): number => {
