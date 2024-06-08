2df11c04eaf29b52e470553fb6e8be17eab56404
francoisdillinger <elmojester@gmail.com>
2024-06-08 18:27:52 -0500

    Refactor ChartToolTip component to update background color

--- src/components/ChartComponents/ChartToolTip.tsx
+++ src/components/ChartComponents/ChartToolTip.tsx
@@ -13,7 +13,7 @@ export default function ChartToolTip({
 	console.log(`Tooltip X:${tooltip.x} , Y:${tooltip.y}`);
 	return (
 		<div
-			className="absolute bg-white rounded-md p-2 text-slate-500 font-light text-xs shadow-2xl"
+			className="absolute bg-gray-100 rounded-md p-2 text-slate-500 font-light text-xs shadow-2xl"
 			style={{
 				display: tooltip.visible ? "block" : "none",
 				left: `${tooltip.x}px`,
