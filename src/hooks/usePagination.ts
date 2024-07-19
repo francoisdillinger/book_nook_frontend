eb61a3cd7ad914f11757809f123fdce89bbf1538
francoisdillinger <elmojester@gmail.com>
2024-07-18 21:26:54 -0500

    refactored usePagination hook

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,60 +1,39 @@
 import { useState, useEffect } from "react";
 
-const getRange = (paginationIndex: number, numOfItems: number) => {
-	const start = (paginationIndex - 1) * numOfItems;
-	const end = paginationIndex * numOfItems - 1;
+const getRange = (paginationIndex: number, itemsPerPage: number) => {
+	const start = (paginationIndex - 1) * itemsPerPage;
+	const end = paginationIndex * itemsPerPage;
 	return [start, end];
 };
 
-const filterByRange = <T>(orders: T[], range: number[]): T[] => {
-	return orders.filter((_, index) => {
-		return index >= range[0] && index <= range[1];
-	});
+const filterByRange = <T>(items: T[], range: number[]): T[] => {
+	return items.slice(range[0], range[1]);
 };
 
 export const usePagination = <T>(listToPaginate: T[], itemsPerPage: number) => {
-	// The following line of code it for future changes where the user might be given
-	// the ability to dynamically choose how many items to view per page.
-	// const [numOfItems, setNumOfItems] = useState<number>(itemsPerPage);
 	const [paginateThisList, setPaginateThisList] = useState<T[]>(listToPaginate);
 	const [pageIndex, setPageIndex] = useState(1);
 	const [paginatedList, setPaginatedList] = useState<T[]>([]);
-	const [increaseDisabled, setIncreaseDisabled] = useState(false);
-	const [decreaseDisabled, setDecreaseDisabled] = useState(true);
-	const totalPages = listToPaginate.length / itemsPerPage;
-
-	const increasePageIndex = () => {
-		if (pageIndex === 10) {
-			setIncreaseDisabled(true);
-		} else {
-			setPageIndex(pageIndex + 1);
-			setIncreaseDisabled(false);
-			setDecreaseDisabled(false);
-		}
-	};
-
-	const decreasePageIndex = () => {
-		if (pageIndex === 1) {
-			setDecreaseDisabled(true);
-		} else {
-			setPageIndex(pageIndex - 1);
-			setDecreaseDisabled(false);
-			setIncreaseDisabled(false);
-		}
-	};
+	const totalPages = Math.ceil(listToPaginate.length / itemsPerPage);
 
 	useEffect(() => {
 		const range = getRange(pageIndex, itemsPerPage);
 		const filteredList = filterByRange(paginateThisList, range);
 		setPaginatedList(filteredList);
-	}, [paginateThisList, pageIndex]);
+	}, [paginateThisList, pageIndex, itemsPerPage]);
+
+	const increasePageIndex = () => {
+		setPageIndex((prevIndex) => Math.min(prevIndex + 1, totalPages));
+	};
+
+	const decreasePageIndex = () => {
+		setPageIndex((prevIndex) => Math.max(prevIndex - 1, 1));
+	};
 
 	return {
 		setPaginateThisList,
 		pageIndex,
 		paginatedList,
-		increaseDisabled,
-		decreaseDisabled,
 		increasePageIndex,
 		decreasePageIndex,
 		totalPages,
