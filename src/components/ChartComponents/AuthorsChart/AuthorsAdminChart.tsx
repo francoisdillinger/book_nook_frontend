381308d9c225f3d10ab57ae4fb7c745bfe9b0aa0
francoisdillinger <elmojester@gmail.com>
2024-07-18 22:14:37 -0500

    imported code needed from duplicated components as I continue refactoring into a single chart component

--- src/components/ChartComponents/AuthorsChart/AuthorsAdminChart.tsx
+++ src/components/ChartComponents/AuthorsChart/AuthorsAdminChart.tsx
@@ -3,11 +3,19 @@ import ChartToolTip from "../ChartToolTip";
 import { TooltipStateType } from "../ChartToolTip";
 import ResponsiveSVGContainer from "../../ResponsiveSVGContainer";
 import { MarginType } from "../AdminChart";
-import AuthorsAdminLineChart from "./AuthorsAdminLineChart";
+import AuthorsAdminLineChart, {
+	CombinedAuthorsOrdersType,
+	combineName,
+	combineOrders,
+	sortOrders,
+	trimAuthorsData,
+} from "./AuthorsAdminLineChart";
 import { authors_data } from "../../../data/authors_data";
 import { AuthorsDataType } from "../../../data/authors_data";
 import AuthorsAdminBarChart from "./AuthorsAdminBarChart";
 import AuthorsAdminPieChart from "./AuthorsAdminPieChart";
+import { usePagination } from "../../../hooks/usePagination";
+import { getFilteredAuthorsData } from "../../../utils/authorsAdminChartUtilities";
 // import {
 // 	AverageBooksType,
 // 	AverageSalesType,
@@ -130,8 +138,105 @@ export default function AuthorsAdminChart({
 	focusedCategory,
 	focusedUser,
 }: AuthorsAdminChartType) {
+	const [orderedAuthorsData, setOrderedAuthorsData] =
+		useState<CombinedAuthorsOrdersType[]>();
+	const [allDates, setAllDates] = useState<string[]>([]);
+	const [allQuantities, setAllQuantinties] = useState<number[]>([]);
+	const {
+		setPaginateThisList,
+		pageIndex,
+		paginatedList,
+		increasePageIndex,
+		decreasePageIndex,
+		totalPages,
+	} = usePagination(orderedAuthorsData ? orderedAuthorsData : [], 10);
+
+	useEffect(() => {
+		const trimmedAuthors = trimAuthorsData(authors_data);
+		const combinedAuthorName = combineName(trimmedAuthors);
+		const combinedOrders = combineOrders(combinedAuthorName);
+		const sortedCombinedOrders = sortOrders(combinedOrders);
+
+		// .authorName: category.authorName,
+		// 	orders: category.orders.sort(
+		// 		(a, b) =>
+		// 			new Date(a.orderDate).getTime() - new Date(b.orderDate).getTime()
+		// 	),
+		// }));
+		const filteredAuthorsChart = getFilteredAuthorsData(
+			timeFilter,
+			sortedCombinedOrders
+		);
+		const flattenedDates = filteredAuthorsChart.flatMap(
+			(author: CombinedAuthorsOrdersType) => {
+				return author.orders.map((order) => order.orderDate);
+			}
+		);
+		const flattenedQuanities = filteredAuthorsChart.flatMap(
+			(author: CombinedAuthorsOrdersType) => {
+				return author.orders.map((order) => order.quantity);
+			}
+		);
+		const uniqueDates = [...new Set(flattenedDates)];
+		const uniqueQuantities = [...new Set(flattenedQuanities)];
+		setAllDates(uniqueDates);
+		setAllQuantinties(uniqueQuantities);
+		setOrderedAuthorsData(filteredAuthorsChart);
+		setPaginateThisList(filteredAuthorsChart);
+		setSelectOptions(filteredAuthorsChart);
+		setHasData(
+			filteredAuthorsChart.reduce(
+				(accumulator, author) => accumulator + author.orders.length,
+				0
+			)
+		);
+	}, [authors_data, timeFilter]);
+
 	return (
 		<React.Fragment>
+			<div className=" bg-gray-100 lg:ml-20 xl:ml-18 rounded-t-lg pt-3 mt-2">
+				<div className="w-1/2 flex justify-between m-auto">
+					<button
+						onClick={decreasePageIndex}
+						className=" p-1 m-1 rounded-md text-sm font-medium bg-white enabled:active:scale-90 enabled:shadow-sm disabled:shadow-none enabled:text-logo disabled:text-gray-400  enabled:cursor-pointer diabled:cursor-default"
+						disabled={pageIndex === 1}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							className="w-6 h-6 stroke-2 stroke-current"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M15.75 19.5 8.25 12l7.5-7.5"
+							/>
+						</svg>
+					</button>
+					<div className="flex justify-center items-center text-gray-500 text-sm font-semibold">
+						Page {pageIndex} of {totalPages}
+					</div>
+					<button
+						onClick={increasePageIndex}
+						className=" p-1 m-1 rounded-md text-sm font-medium bg-white enabled:active:scale-90 enabled:shadow-sm disabled:shadow-none enabled:text-logo disabled:text-gray-400  enabled:cursor-pointer diabled:cursor-default"
+						disabled={pageIndex === totalPages}
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							className="w-6 h-6 stroke-2 stroke-current"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="m8.25 4.5 7.5 7.5-7.5 7.5"
+							/>
+						</svg>
+					</button>
+				</div>
+			</div>
 			<div className="flex flex-wrap lg:ml-20 xl:ml-18">
 				<div className="bg-gray-100 rounded-b-lg pt-2 w-full">
 					{/* Note to self: Yes, this component rerenders when tooltip appears and disappears */}
@@ -141,6 +246,9 @@ export default function AuthorsAdminChart({
 							<ResponsiveSVGContainer>
 								<AuthorsAdminLineChart
 									//height and width are provided by the <ResponsiveSVGContainer>
+									paginatedList={paginatedList}
+									allDates={allDates}
+									allQuantities={allQuantities}
 									authors={chartData}
 									margin={margin}
 									timeFilter={timeFilter}
@@ -163,6 +271,7 @@ export default function AuthorsAdminChart({
 					<ResponsiveSVGContainer>
 						<AuthorsAdminBarChart
 							//height and width are provided by the <ResponsiveSVGContainer>
+							paginatedList={paginatedList}
 							margin={margin}
 							timeFilter={timeFilter}
 							tooltip={tooltip}
@@ -179,6 +288,8 @@ export default function AuthorsAdminChart({
 					<ResponsiveSVGContainer>
 						<AuthorsAdminPieChart
 							//height and width are provided by the <ResponsiveSVGContainer>
+							paginatedList={paginatedList}
+							pageIndex={pageIndex}
 							timeFilter={timeFilter}
 							tooltip={tooltip}
 							setTooltip={setTooltip}
