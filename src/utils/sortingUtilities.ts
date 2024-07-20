2b3bbec8a1e4c904905fb2f37c62a5a5a292d599
francoisdillinger <elmojester@gmail.com>
2024-07-20 03:35:15 -0500

    Refactoring!!!!!!!!!

--- src/utils/sortingUtilities.ts
+++ src/utils/sortingUtilities.ts
@@ -8,17 +8,46 @@
 // ];
 
 import { CombinedAuthorsOrdersType } from "../components/ChartComponents/AuthorsChart/AuthorsAdminLineChart";
+import { CombinedChartDataOrdersType } from "../components/ChartComponents/LineChart";
+
+// export const sortListBySelectOption = (
+// 	list: CombinedAuthorsOrdersType[],
+// 	sortOption: string
+// ): CombinedAuthorsOrdersType[] => {
+// 	switch (sortOption) {
+// 		case "Sort: A-Z":
+// 			return list.sort((a, b) => a.authorName.localeCompare(b.authorName));
+
+// 		case "Sort: Z-A":
+// 			return list.sort((a, b) => b.authorName.localeCompare(a.authorName));
+
+// 		case "Total Items: Ascending":
+// 			return list.sort((a, b) => a.totalItems - b.totalItems);
+
+// 		case "Total Items: Descending":
+// 			return list.sort((a, b) => b.totalItems - a.totalItems);
+
+// 		case "Total $ Amount: Ascending":
+// 			return list.sort((a, b) => a.totalAmount - b.totalAmount);
+
+// 		case "Total $ Amount: Descending":
+// 			return list.sort((a, b) => b.totalAmount - a.totalAmount);
+
+// 		default:
+// 			return list;
+// 	}
+// };
 
 export const sortListBySelectOption = (
-	list: CombinedAuthorsOrdersType[],
+	list: CombinedChartDataOrdersType[],
 	sortOption: string
-): CombinedAuthorsOrdersType[] => {
+): CombinedChartDataOrdersType[] => {
 	switch (sortOption) {
 		case "Sort: A-Z":
-			return list.sort((a, b) => a.authorName.localeCompare(b.authorName));
+			return list.sort((a, b) => a.name.localeCompare(b.name));
 
 		case "Sort: Z-A":
-			return list.sort((a, b) => b.authorName.localeCompare(a.authorName));
+			return list.sort((a, b) => b.name.localeCompare(a.name));
 
 		case "Total Items: Ascending":
 			return list.sort((a, b) => a.totalItems - b.totalItems);
