c65af0587968d322b8751dac8ebd9e5b53fe3c2b
francoisdillinger <elmojester@gmail.com>
2024-07-22 00:36:42 -0500

    added functionality for remodeling users data to universal chart data

--- src/utils/transformData.ts
+++ src/utils/transformData.ts
@@ -9,7 +9,11 @@ import {
 	CategoriesDataType,
 	TrimmedCategoriesDataType,
 	ChartDataType,
+	TrimmedUserType,
+	UserOrderData,
 } from "./junk";
+import { UsersType } from "../data/users";
+import { filterOutEmptyCategories, filterOutInactiveUsers } from "./filterData";
 
 const trimAuthorsData = (authors: AuthorsDataType): TrimmedAuthorsDataType => {
 	return {
@@ -79,8 +83,78 @@ const categoriesDataToChartData = (
 export const transformCategoriesToChartDataFormat = (
 	chartData: CategoriesDataType
 ): CombinedChartDataOrdersType[] => {
-	return combineOrders(
-		categoriesDataToChartData(trimCategoriesData(chartData))
+	return filterOutEmptyCategories(
+		combineOrders(categoriesDataToChartData(trimCategoriesData(chartData)))
+	);
+};
+
+export const trimUserData = (users: UsersType): TrimmedUserType[] => {
+	return users.data.users.map((user) => user);
+};
+
+export const aggregateOrdersByOrderId = (
+	users: TrimmedUserType[]
+): TrimmedUserType[] => {
+	return users.map((user) => {
+		// Initialize a Record to track orders by their orderId and aggregate their totals.
+		const aggregationById: Record<string, UserOrderData> = {};
+
+		user.orders.forEach((order) => {
+			// If an order with the same orderId already exists in the Record, aggregate its totals.
+			if (aggregationById[order.orderId]) {
+				aggregationById[order.orderId].quantity += order.quantity;
+				aggregationById[order.orderId].orderAmount = parseFloat(
+					(
+						aggregationById[order.orderId].orderAmount + order.orderAmount
+					).toFixed(2)
+				);
+			} else {
+				// If an order with the same orderId does not exist, create a new entry in the Record.
+				aggregationById[order.orderId] = {
+					orderDate: order.orderDate,
+					quantity: order.quantity,
+					orderId: order.orderId,
+					orderAmount: order.orderAmount,
+				};
+			}
+		});
+
+		const aggregatedOrders = Object.values(aggregationById).sort(
+			(a, b) =>
+				new Date(a.orderDate).getTime() - new Date(b.orderDate).getTime()
+		);
+
+		return {
+			userName: user.userName,
+			orders: aggregatedOrders,
+		};
+	});
+};
+
+export const combineUserOrders = (
+	chartData: TrimmedUserType[]
+): CombinedChartDataOrdersType[] => {
+	return chartData.map((item) => {
+		let totalAmount = 0;
+		let totalItems = 0;
+		return {
+			name: item.userName,
+			orders: item.orders.map((order) => {
+				totalAmount += order.orderAmount;
+				totalItems += order.quantity;
+				return { ...order, uniqueId: uuidv4() };
+			}),
+			totalAmount: parseFloat(totalAmount.toFixed(2)),
+			totalItems: totalItems,
+		};
+	});
+};
+
+export const transformUsersToChartDataFormat = (
+	chartData: UsersType
+): CombinedChartDataOrdersType[] => {
+	return combineUserOrders(
+		aggregateOrdersByOrderId(filterOutInactiveUsers(trimUserData(chartData)))
 	);
 };
 
