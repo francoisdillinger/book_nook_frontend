52f89f0863986f5e44b58620f3697badc3892b79
francoisdillinger <elmojester@gmail.com>
2024-07-22 22:00:17 -0500

    refactor: Update SimpleSelect component to use generic type for options and onChange

--- src/components/SimpleSelect.tsx
+++ src/components/SimpleSelect.tsx
@@ -1,37 +1,34 @@
+import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
 import React, { Dispatch, SetStateAction, useState } from "react";
+import { ChartSortType } from "../features/chart/chartSortOptionsSlice";
 
-type SimpleSelectType = {
-	options: string[];
-	setSortOption: Dispatch<SetStateAction<string>>;
+type SimpleSelectProps<T> = {
+	options: T[];
+	onChange: (selectedOption: T) => void;
 };
 
-export default function SimpleSelect({
+export default function SimpleSelect<T extends string>({
 	options,
-	setSortOption,
-}: SimpleSelectType) {
-	// const [sortOption, setSortOption] = useState<{ sortOption: string }>({
-	// 	sortOption: options[0],
-	// });
-
-	// const selectOptionsHandler = (
-	// 	event: React.ChangeEvent<HTMLSelectElement>
-	// ) => {
-	// 	setSortOption({
-	// 		sortOption: event.target.value,
-	// 	});
-	// };
-
-	// console.log("Option: ", sortOption.sortOption);
+	onChange,
+}: SimpleSelectProps<T>) {
+	const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		onChange(event.target.value as T);
+	};
 
 	return (
 		<div className="h-full">
 			<select
 				id="book-selector"
-				className="h-full w-42 md:w-fitfont-semibold form-select block py-2.5 px-3 border border-gray-300 bg-white text-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-blue-300 focus:border-blue-300 sm:text-sm"
-				onChange={(event) => setSortOption(event.target.value)}
+				className="h-full w-42 md:w-fit font-semibold form-select block py-2.5 px-3 border border-gray-300 bg-white text-gray-400 rounded-md shadow-sm focus:outline-none focus:ring-blue-300 focus:border-blue-300 sm:text-sm"
+				onChange={handleChange}
 			>
 				{options.map((option) => (
-					<option key={option}>{option}</option>
+					<option
+						key={option}
+						value={option}
+					>
+						{option}
+					</option>
 				))}
 			</select>
 		</div>
