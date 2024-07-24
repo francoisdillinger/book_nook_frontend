99a8ef0e76e8a3381bf87ceaad5f926c2474beeb
francoisdillinger <elmojester@gmail.com>
2024-07-24 02:18:29 -0500

    refactor: Remove unnecessary code and dependencies in LineChart component

--- src/components/ChartComponents/LineChart.tsx
+++ src/components/ChartComponents/LineChart.tsx
@@ -13,109 +13,6 @@ import {
 	getUniqueQuantities,
 } from "../../utils/getData";
 
-// let totalAmount = 0;
-// let totalItems = 0;
-// totalAmount += order.orderAmount;
-// totalItems += order.quantity;
-// totalAmount: parseFloat(totalAmount.toFixed(2)),
-// totalItems: totalItems,
-
-// export const combineOrders = (data: ChartDataType[]) => {
-// 	console.log("data: ", data);
-// };
-
-// export const combineOrders = (
-// 	authors: ChartDataType[]
-// ): CombinedChartDataOrdersType[] => {
-// 	let totalAmount = 0;
-// 	let totalItems = 0;
-// 	return authors.map((author) => {
-// 		return {
-// 			name: author.name,
-// 			orders: author.books
-// 				.map((book) => {
-// 					return book.bookOrders.map((order) => {
-// 						totalAmount += order.orderAmount;
-// 						totalItems += order.quantity;
-// 						return {
-// 							bookTitle: book.bookTitle,
-// 							...order,
-// 							uniqueId: uuidv4(),
-// 						};
-// 					});
-// 				})
-// 				.flat(),
-// 			totalAmount: parseFloat(totalAmount.toFixed(2)),
-// 			totalItems: totalItems,
-// 		};
-// 	});
-// };
-
-// data.map((item) => {
-//     return {
-//         name: item.name,
-//         orders: item.books.flatMap((book) => {
-//             if (!Array.isArray(book.bookOrders)) {
-//                 console.error(
-//                     `Expected bookOrders to be an array, but got ${typeof book.bookOrders}`
-//                 );
-//                 return [];
-//             }
-//             return book.bookOrders.map((order) => {
-//                 return {
-//                     bookTitle: book.bookTitle,
-//                     ...order,
-//                     uniqueId: uuidv4(),
-//                 };
-//             });
-//         }),
-//     };
-// });
-
-// export const combineName = (
-// 	authors: TrimmedAuthorsDataType
-// ): CombinedAuthorNameType[] => {
-// 	return authors.authors.map((author) => {
-// 		return {
-// 			authorName: author.authorFirstName + " " + author.authorLastName,
-// 			books: author.books,
-// 		};
-// 	});
-// };
-
-// export const sortOrders = (
-// 	data: CombinedChartDataOrdersType[]
-// ): CombinedChartDataOrdersType[] => {
-// 	return data.map((item) => {
-// 		return {
-// 			...item,
-// 			orders: item.orders.sort((a, b) => {
-// 				return (
-// 					new Date(a.orderDate).getTime() - new Date(b.orderDate).getTime()
-// 				);
-// 			}),
-// 		};
-// 	});
-// };
-
-// type TrimmedAuthorsDataType = {
-// 	authors: {
-// 		authorFirstName: string;
-// 		authorLastName: string;
-// 		books: {
-// 			bookTitle: string;
-// 			bookOrders: {
-// 				orderId: string;
-// 				userId: string;
-// 				bookId: string;
-// 				quantity: number;
-// 				orderDate: string;
-// 				orderAmount: number;
-// 			}[];
-// 		}[];
-// 	}[];
-// };
-
 export type CombinedAuthorNameType = {
 	authorName: string;
 	books: {
@@ -147,11 +44,6 @@ export type CombinedChartDataOrdersType = {
 	}[];
 };
 
-// export type ReducedAuthorsDataType = {
-// 	authorName: string;
-// 	totalBooksOrdered: number;
-// };
-
 export type ReducedChartDataType = {
 	name: string;
 	totalBooksOrdered: number;
@@ -160,38 +52,20 @@ export type ReducedChartDataType = {
 type LineChartType = {
 	paginatedList: CombinedChartDataOrdersType[];
 	orderedChartsData: CombinedChartDataOrdersType[] | undefined;
-	// allDates: string[];
-	// allQuantities: number[];
-	// margin: MarginType;
-	// timeFilter: string;
 	width?: number;
 	height?: number;
-	// tooltip: TooltipStateType;
-	// setTooltip: Function;
-	// authors: AuthorsDataType;
-	// colorScale: Function;
 	hasData: number;
-	// setHasData: Function;
-	// setSelectOptions: Function;
-	// focusedCategory: string;
-	// doesToolTipOverflowWindow: Function;
 };
 
 export default function LineChart({
 	orderedChartsData,
-	// allDates,
-	// allQuantities,
 	paginatedList,
-	// margin,
+
 	width = 0,
 	height = 0,
-	// tooltip,
-	// setTooltip,
-	// colorScale,
+
 	hasData,
-}: // focusedCategory,
-// doesToolTipOverflowWindow,
-LineChartType) {
+}: LineChartType) {
 	const svgWidth = width;
 	const svgHeight = height;
 	const graphHeight = svgHeight - MARGIN.top - MARGIN.bottom;
@@ -200,11 +74,6 @@ LineChartType) {
 	const graphLineChartRef = useRef<SVGSVGElement>(null);
 	const [allDates, setAllDates] = useState<string[]>([]);
 	const [allQuantities, setAllQuantinties] = useState<number[]>([]);
-	// const focusedDataPoint = useSelector(
-	// 	(state: RootState) => state.highlightData.focusedDataPoint
-	// );
-	// const dispatch = useDispatch();
-	// const tooltip = useSelector((state: RootState) => state.ChartToolTip);
 
 	useEffect(() => {
 		const flattenedDates = getFlattenedDates(
@@ -231,7 +100,7 @@ LineChartType) {
 		() => d3.extent(parsedDates) as [Date, Date] | [undefined, undefined],
 		[parsedDates]
 	);
-	// console.log("Date Extent: ", dateExtent);
+
 	const domain = useMemo(
 		() =>
 			dateExtent[0] && dateExtent[1] ? dateExtent : [new Date(), new Date()],
@@ -305,16 +174,12 @@ LineChartType) {
 						paginatedList != undefined &&
 						paginatedList.map((dataPoint, index) => {
 							const color = colorScale(index.toString());
-							// console.log("Color: ", color);
-							// console.log("Category: ", category);
 							const linePath = theLine(
 								dataPoint.orders.map((order) => [
 									new Date(order.orderDate).getTime(), // Convert Date string to Date object and then get the time
 									order.quantity,
 								])
 							);
-							// console.log("Line Path: ", linePath);
-							// console.log("Focused Category: ", focusedCategory);
 
 							// This prevents framer motion errors from blowing up
 							// the console when no orders are present as it tries
@@ -331,8 +196,6 @@ LineChartType) {
 										color={color}
 									/>
 									{dataPoint.orders.map((order) => {
-										// console.log("Order Quantity: ", order.quantity);
-										// console.log("cy: ", y(order.quantity));
 										return (
 											<LineChartCircle
 												order={order}
