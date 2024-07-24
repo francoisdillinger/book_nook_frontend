177c06ca3aed98d9d77a034d4187f8ecea23f040
francoisdillinger <elmojester@gmail.com>
2024-07-24 02:16:31 -0500

    refactor: Remove unused code and dependencies in PieChart component

--- src/components/ChartComponents/PieChart.tsx
+++ src/components/ChartComponents/PieChart.tsx
@@ -7,110 +7,39 @@ import { RootState } from "../../app/store";
 import { useSelector } from "react-redux";
 import PieChartPath from "./PieChartPath";
 
-// const reduceOrderQuantities = (
-// 	authors: CombinedAuthorsOrdersType[]
-// ): ReducedAuthorsDataType[] => {
-// 	return authors.map((author) => {
-// 		return {
-// 			authorName: author.authorName,
-// 			totalBooksOrdered: author.orders.reduce(
-// 				(accumulator, order) => accumulator + order.quantity,
-// 				0
-// 			),
-// 		};
-// 	});
-// };
-
-// const filterOutInactiveUsers = (
-// 	users: ProcessedUserType[]
-// ): ProcessedUserType[] => {
-// 	return users.filter((user) => user.orders.length > 0);
-// };
-
 type PieChartType = {
 	paginatedList: CombinedChartDataOrdersType[];
-	// pageIndex: number;
-	// timeFilter: string;
 	width?: number;
 	height?: number;
-	// tooltip: TooltipStateType;
-	// setTooltip: Function;
-	// authors: AuthorsDataType;
-	// colorScale: Function;
 	hasData: number;
-	// focusedCategory: string;
-	// doesToolTipOverflowWindow: Function;
 };
 
 export default function PieChart({
 	paginatedList,
-	// pageIndex,
-	// timeFilter,
 	width = 0,
 	height = 0,
-	// tooltip,
-	// setTooltip,
-	// authors,
-	// colorScale,
 	hasData,
-}: // focusedCategory,
-// doesToolTipOverflowWindow,
-PieChartType) {
+}: PieChartType) {
 	const svgWidth = width;
 	const svgHeight = height;
 	const graphHeight = svgHeight;
 	const graphWidth = svgWidth;
-	//
-	// const [reducedAuthorsData, setReducedAuthorsData] =
-	// 	useState<ReducedAuthorsDataType[]>();
 	const [totalOrderCount, setTotalOrderCount] = useState(0);
 	const focusedDataPoint = useSelector(
 		(state: RootState) => state.highlightData.focusedDataPoint
 	);
 	const [key, setKey] = useState(0);
-	// const dispatch = useDispatch();
-	// const tooltip = useSelector((state: RootState) => state.ChartToolTip);
 
 	useEffect(() => {
-		// const trimmedAuthors = trimAuthorsData(authors);
-		// const combinedAuthorName = combineName(trimmedAuthors);
-		// const combinedOrders = combineOrders(combinedAuthorName);
-		// const sortedCombinedOrders = sortOrders(combinedOrders);
-		// const filteredAuthorsChart = getFilteredAuthorsData(
-		// 	timeFilter,
-		// 	sortedCombinedOrders
-		// );
-		// const trimmedCategories = trimCategoriesData(categories);
-		// const reformattedCategories = reformatCategoriesBooks(trimmedCategories);
-		// const filteredCategories = filterOutEmptyCategories(reformattedCategories);
-		// console.log("Empty Filtered: ", filteredCategories);
-		// const authorArray = filteredCategories.categories.map((author) => ({
-		// 	authorName: author.authorName,
-		// 	orders: author.orders.sort(
-		// 		(a, b) =>
-		// 			new Date(a.orderDate).getTime() - new Date(b.orderDate).getTime()
-		// 	),
-		// }));
-		// const timeFilteredCategories = getFilteredCategoriesData(
-		// 	timeFilter,
-		// 	authorArray
-		// );
-		// console.log("Time Filtered: ", timeFilteredCategories);
-		// const reducedData = reduceOrderQuantities(timeFilteredCategories);
 		setTotalOrderCount(
 			paginatedList.reduce(
 				(accumulator, data) => accumulator + data.totalItems,
 				0
 			)
 		);
-		// setReducedCategoriesData(reducedData);
-		// setReducedAuthorsData(reduceOrderQuantities(filteredAuthorsChart));
 		setKey((prevKey) => prevKey + 1);
 	}, [paginatedList]);
-	// console.log("Total Order Count: ", totalOrderCount);
-	// console.log("PageKey: ", key);
-	// console.log("Key: ", key);
-	// console.log("Total Order Count: ", totalOrderCount);
+
 	const pie = useMemo(() => {
 		return d3
 			.pie<CombinedChartDataOrdersType>()
@@ -212,7 +141,6 @@ PieChartType) {
 							paginatedList != undefined &&
 							pie(paginatedList)!.map((item, index) => {
 								const color = colorScale(index.toString());
-								// console.log("Category: ", item);
 								// This is to override a bug where orders of 0 are still shown
 								// on the chart, but filtering them changes the index #'s so
 								// the colors change as well. I'll come back to this.
