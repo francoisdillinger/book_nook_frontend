091cf3284f0b54a6b35595f1e37c3971ee72a2ab
francoisdillinger <elmojester@gmail.com>
2024-07-25 01:34:16 -0500

    refactor: Update import statement for MarginType in AuthorsAdminLineChart.tsx

--- src/components/ChartComponents/AuthorsChart/AuthorsAdminLineChart.tsx
+++ src/components/ChartComponents/AuthorsChart/AuthorsAdminLineChart.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 import { TooltipStateType } from "../ChartToolTip";
 import XAxis from "../../XAxis";
 import YAxis from "../../YAxis";
-import { MarginType } from "../../AdminHome";
+import { MarginType } from "../../AdminHomePage";
 import { AuthorsDataType } from "../../../data/authors_data";
 // import { getFilteredAuthorsData } from "../../../utils/authorsAdminChartUtilities";
 // import CategoriesChartReactSelect from "../CategoriesChart/CategoriesChartReactSelect";
