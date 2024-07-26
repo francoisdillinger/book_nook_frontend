4c9c7b2145c00790a4c9ceae0a5f6fe946ded69f
francoisdillinger <elmojester@gmail.com>
2024-07-26 00:04:14 -0500

    feat: Add react-router-dom for navigation
    Refactor the Navbar component to use react-router-dom for navigation. Import the Link component from react-router-dom and replace anchor tags with Link components where appropriate. This change improves the user experience by providing a smoother navigation flow within the application.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import useMenuVisibility from "../hooks/useMenuVisibility";
 import useDropDownVisibility from "../hooks/useDropDownVisibility";
 import SearchBar from "./SearchBar";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
 	const [loggedIn, setLoggedIn] = useState(false);
@@ -21,7 +22,10 @@ export default function Navbar() {
 		<nav className="bg-gray-100">
 			<div className="max-w-[1600px] mx-auto px-4">
 				<div className="flex justify-between">
-					<div className="flex items-center">
+					<Link
+						className="flex items-center"
+						to={"/"}
+					>
 						<a
 							href=""
 							className="px-2 py-2"
@@ -38,7 +42,7 @@ export default function Navbar() {
 							</h2>
 							<p className="text-slate-500 text-xs">Where Stories Live</p>
 						</div>
-					</div>
+					</Link>
 					<div className="hidden md:flex items-center w-1/2">
 						<SearchBar
 							options={["All", "Author", "Title"]}
@@ -155,6 +159,33 @@ export default function Navbar() {
 														</svg>
 														<span className="px-1">Settings</span>
 													</a>
+												</li>
+												<li>
+													<Link
+														to={"/admin"}
+														className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+													>
+														<svg
+															xmlns="http://www.w3.org/2000/svg"
+															fill="none"
+															viewBox="0 0 24 24"
+															strokeWidth={1.5}
+															stroke="currentColor"
+															className="w-6 h-6 inline"
+														>
+															<path
+																strokeLinecap="round"
+																strokeLinejoin="round"
+																d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 011.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.56.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.893.149c-.425.07-.765.383-.93.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 01-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.397.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 01-.12-1.45l.527-.737c.25-.35.273-.806.108-1.204-.165-.397-.505-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.107-1.204l-.527-.738a1.125 1.125 0 01.12-1.45l.773-.773a1.125 1.125 0 011.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894z"
+															/>
+															<path
+																strokeLinecap="round"
+																strokeLinejoin="round"
+																d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+															/>
+														</svg>
+														<span className="px-1">Admin</span>
+													</Link>
 												</li>
 												<li
 													onClick={() => {
