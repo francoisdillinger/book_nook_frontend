bc86a508297af04b7e0d517fe15fc3ad342c452e
francoisdillinger <elmojester@gmail.com>
2024-07-26 00:04:06 -0500

    feat: Implement react-router-dom for routing
    Refactor the App component to implement react-router-dom for routing. Remove unnecessary commented-out code and use the Outlet component from react-router-dom to render the matched route component. This change improves the organization and maintainability of the codebase.

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,36 @@ import App from "./App.tsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./app/store.ts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage.tsx";
+import SearchResultsPage from "./pages/SearchResultPage.tsx";
+import AdminHomePage from "./pages/AdminHomePage.tsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />, // App as the layout
+		children: [
+			{
+				path: "/",
+				element: <HomePage />,
+			},
+			{
+				path: "/search",
+				element: <SearchResultsPage />,
+			},
+			{
+				path: "/admin",
+				element: <AdminHomePage />,
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<RouterProvider router={router} />
 		</Provider>
 	</React.StrictMode>
 );
